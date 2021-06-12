325eed9cc724ac6c0a9e8b7d24b1f3445455838a
Ken <ken.aleksander@gmail.com>
2021-06-12 23:59:37 +0200

    tried resizing codeExample

--- website/components/code/CodeExample.tsx
+++ website/components/code/CodeExample.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import { v4 as uuidv4 } from "uuid";
 import RenderExample from "examples";
+
+import { v4 as uuid } from "uuid";
+import { useMount } from "react-use";
 
 const Div = styled.div`
   padding: 3rem;
@@ -10,11 +12,28 @@ const Div = styled.div`
   border-bottom: none;
 `;
 
+const Handle = styled.span`
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  color: black;
+  background: black;
+`;
+
 const CodeExample = ({ component }) => {
+  const [width, setWidth] = useState(200);
+  const divId = useRef(component);
+
+  useMount(() =>
+    setWidth(document.getElementById(divId.current).offsetWidth - 6 * 16)
+  );
+
   return (
-    <Div>
-      <RenderExample component={component} />
-    </Div>
+    <>
+      <Div id={component}>
+        <RenderExample component={component} />
+      </Div>
+    </>
   );
 };
 
