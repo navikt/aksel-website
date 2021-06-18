f5ed53768f50eeda95b847a3356e05dbdedb0926
Ken <ken.aleksander@gmail.com>
2021-06-18 20:20:51 +0200

    fjernet studio rewrite

--- website/next.config.js
+++ website/next.config.js
@@ -13,18 +13,18 @@ Object.keys(packageJson.dependencies).forEach((key) => {
 /**
  * Allows us to run the sanity content studio on subpath "/studio"
  */
-const STUDIO_REWRITE = {
+/* const STUDIO_REWRITE = {
   source: "/studio/:path*",
   destination:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3333/studio/:path*"
       : "/studio/index.html",
-};
+}; */
 
 const withTM = require("next-transpile-modules")(modules);
 
 module.exports = withTM({
-  rewrites: () => [STUDIO_REWRITE],
+  /* rewrites: () => [STUDIO_REWRITE], */
   productionBrowserSourceMaps: true,
   // Makes sure we can load images form cdn
   images: {
