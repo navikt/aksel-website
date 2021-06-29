d2a10d6faf1f25ebb8b26663486d9d4891570948
Ken <ken.aleksander@gmail.com>
2021-06-29 14:22:09 +0200

    oppdatert template for komponenter

--- sanity/initialValueTemplates.js
+++ sanity/initialValueTemplates.js
@@ -7,6 +7,67 @@ export default [
     id: "ds_component_page_template",
     title: "Komponentside template",
     schemaType: "ds_component_page",
-    value: {},
+    value: () => ({
+      status: "wip",
+      usage: [
+        ...getHeading("Slik brukes komponeten"),
+        ...getHeading("Anatomien"),
+        ...getHeading("Komponent eksempel 1"),
+        { _type: "code_example" },
+        ...getHeading("Komponent eksempel 2"),
+        { _type: "code_example" },
+        { _type: "do_dont" },
+        ...getHeading("Universell utforming"),
+        { _type: "uu_interaction" },
+        { _type: "linker" },
+        ...getHeading("Designavgjørelse"),
+        ...getHeading("Bruk i det fri"),
+      ],
+      design: [
+        { _type: "linker" },
+        ...getHeading("Anatomien"),
+        ...getHeading("Bruke komponenten i Figma"),
+        { _type: "do_dont" },
+      ],
+      development: [
+        { _type: "linker" },
+        { _type: "code_example" },
+        { _type: "prop_table" },
+        { _type: "changelog" },
+        { _type: "linker" },
+      ],
+      accessibility: [
+        ...getHeading("Vurderin av universell utforming"),
+        ...getHeading("Referanser"),
+        ...getHeading("Teste universell utforming"),
+      ],
+    }),
   }),
+];
+
+const getHeading = (heading) => [
+  {
+    _type: "block",
+    children: [
+      {
+        _type: "span",
+        marks: [],
+        text: heading,
+      },
+    ],
+    markDefs: [],
+    style: "h2",
+  },
+  {
+    _type: "block",
+    children: [
+      {
+        _type: "span",
+        marks: [],
+        text: "Et laborum deserunt consectetur enim cupidatat sit laborum ullamco.",
+      },
+    ],
+    markDefs: [],
+    style: "normal",
+  },
 ];
