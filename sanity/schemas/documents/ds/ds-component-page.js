99a0e69bc21e7a2939cd5f7e805c6544a82b9b76
Ken <ken.aleksander@gmail.com>
2021-07-08 11:17:42 +0200

    la til legacy status-basge

--- sanity/schemas/documents/ds/ds-component-page.js
+++ sanity/schemas/documents/ds/ds-component-page.js
@@ -50,6 +50,7 @@ export default {
           { value: "published", title: "Publisert" },
           { value: "beta", title: "Beta" },
           { value: "wip", title: "WIP" },
+          { value: "legacy", title: "Legacy / deprecated" },
         ],
         layout: "radio",
       },
