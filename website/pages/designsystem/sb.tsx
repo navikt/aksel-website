50eba1a8fe19ced375a6e3f274e4298a95c4b3ef
Ken <ken.aleksander@gmail.com>
2021-07-10 14:45:29 +0200

    storybook

---
+++ website/pages/designsystem/sb.tsx
@@ -0,0 +1,13 @@
+const StorybookFrame = (): JSX.Element => {
+  return (
+    <div>
+      {/* <iframe
+        src={"http://localhost:6006/iframe.html?id=example-button--primary"}
+        height="500px"
+        width="100%"
+      /> */}
+    </div>
+  );
+};
+
+export default StorybookFrame;
