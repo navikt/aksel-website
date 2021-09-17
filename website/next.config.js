c6fec5a8068209cb3cce759cf0770aef872349ca
Ken <ken.aleksander@gmail.com>
2021-09-17 16:01:19 +0200

    oppdaterteconfig

--- website/next.config.js
+++ website/next.config.js
@@ -1,50 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-const packageJson = require("./package.json");
-const modules = [];
-// Remember to remove deps in package.json when less is removed
-const withLess = require("next-with-less");
 
-Object.keys(packageJson.dependencies).forEach((key) => {
-  /**
-   * Nextjs does not as of june 2021 support esm import/export syntax
-   * TODO: Remove this when this issue is fixed
-   *  */
-  if (
-    key.startsWith("@navikt/") ||
-    key === "examples" ||
-    key.startsWith("nav-frontend-")
-  ) {
-    modules.push(key);
-  }
-});
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-const withTM = require("next-transpile-modules")(modules);
-
-module.exports = withTM(
-  withLess({
-    // TODO: Fikse slik at /storybook -> /storybook/index.html (oppdatert rege i codeBlock også da)
-    /* rewrites: async () => {
-      return [
-        {
-          source: "/storybook/:page",
-          destination: "/storybook/index.html",
-        },
-      ];
-    }, */
-
-    productionBrowserSourceMaps: true,
-    // Makes sure we can load images form cdn
-    images: {
-      domains: ["cdn.sanity.io"],
-    },
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
-
-      return config;
-    },
-  })
-);
+    return config;
+  },
+};
