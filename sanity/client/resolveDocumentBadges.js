83b17f1ab1e7e42837640fdc09c13d2f0c31cb21
Ken <ken.aleksander@gmail.com>
2021-09-21 14:25:53 +0200

    badge fix

--- sanity/client/resolveDocumentBadges.js
+++ sanity/client/resolveDocumentBadges.js
@@ -3,6 +3,6 @@ import defaultResolve from "part:@sanity/base/document-badges";
 
 import { updatedBadge } from "../badges/UpdatedBadge";
 
-export default function resolveDocumentBadges() {
+export default function resolveDocumentBadges(props) {
   return [...defaultResolve(props), updatedBadge];
 }
