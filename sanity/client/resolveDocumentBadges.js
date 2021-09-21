c6d5640a8683559b8285d53e7525f64cb61e0bdd
Ken <ken.aleksander@gmail.com>
2021-09-21 14:24:14 +0200

    oppdatert laypout

--- sanity/client/resolveDocumentBadges.js
+++ sanity/client/resolveDocumentBadges.js
@@ -1,8 +1,8 @@
 /* Default badge */
-/* import defaultResolve from "part:@sanity/base/document-badges"; */
+import defaultResolve from "part:@sanity/base/document-badges";
 
 import { updatedBadge } from "../badges/UpdatedBadge";
-/* ...defaultResolve(props) */
+
 export default function resolveDocumentBadges() {
-  return [updatedBadge];
+  return [...defaultResolve(props), updatedBadge];
 }
--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -2,21 +2,28 @@ import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
 /* Schemas */
+/* Documents */
+import metadata from "./documents/admin/metadata";
+import editors from "./documents/editors";
+import navigation, { dropdown, link } from "./documents/navigation";
+/*--  Designsystem */
+import designsystem_component_page from "./documents/designsystem/ds-component-page";
+import designsystem_article_page from "./documents/designsystem/ds-article-page";
+import designsystem_tabbed_article_page from "./documents/designsystem/ds-tabbed-article-page";
+/*--  God Praksis */
+import god_praksis_article_page from "./documents/god-praksis/gp-article-page";
+/*--  Admin */
+
+/*  Components */
 import block_content from "./block-content";
 import code_example, { example } from "./objects/code-example";
 import prop_table, { prop } from "./objects/proptable";
 import changelog, { change } from "./objects/changelog";
-import designsystem_component_page from "./documents/designsystem/ds-component-page";
-import designsystem_article_page from "./documents/designsystem/ds-article-page";
-import designsystem_tabbed_article_page from "./documents/designsystem/ds-tabbed-article-page";
 import do_dont, { doDont } from "./objects/do-dont";
 import linker, { links } from "./objects/linker";
-import persons from "./documents/persons";
 import uu_interactions, { keyboardTable } from "./objects/uu-interaction";
 import picture from "./objects/image";
 import alert from "./objects/alert";
-import navigation, { dropdown, link } from "./documents/navigation";
-import metadata from "./documents/metadata";
 
 export default createSchema({
   name: "default",
@@ -24,6 +31,7 @@ export default createSchema({
     designsystem_component_page,
     designsystem_article_page,
     designsystem_tabbed_article_page,
+    god_praksis_article_page,
     code_example,
     example,
     block_content,
@@ -35,7 +43,7 @@ export default createSchema({
     doDont,
     linker,
     links,
-    persons,
+    editors,
     uu_interactions,
     keyboardTable,
     picture,
