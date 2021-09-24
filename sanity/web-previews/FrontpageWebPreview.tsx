f0f1063c3cf69727188d23c0d083ea36585ea378
Ken <ken.aleksander@gmail.com>
2021-09-24 15:00:24 +0200

    fikset preview

--- sanity/web-previews/FrontpageWebPreview.tsx
+++ sanity/web-previews/FrontpageWebPreview.tsx
@@ -4,7 +4,7 @@ import { WebPreviewWrapper } from "./WebPreviewWrapper";
 export const FrontpageWebPreview = (ctx: any) => {
   const previewUrl = `/?preview=true`;
   /* const webUrl = "https://verktoykasse-prototype.dev.nav.no"; */
-  const webUrl = "https://verktoykassen-ob1sg07rc-kenajoh.vercel.app";
+  const webUrl = "https://verktoykassen-kenajoh.vercel.app";
   const url =
     process.env.NODE_ENV === "production"
       ? webUrl + previewUrl
