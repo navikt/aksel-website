9f7254940a7c8a93320b986d5fdc2564509856bf
Ken <ken.aleksander@gmail.com>
2021-10-10 20:15:48 +0200

    oppdatert komps

--- website/component-examples.ts
+++ website/component-examples.ts
@@ -1,4 +1,19 @@
-import ButtonAllVariants from "./pages/examples/button/AllVariants";
 import ButtonDanger from "./pages/examples/button/Danger";
+import ButtonAllVariants from "./pages/examples/button/AllVariants";
+import ButtonDisabled from "./pages/examples/button/Disabled";
+import ButtonPrimary from "./pages/examples/button/Primary";
+import ButtonSecondary from "./pages/examples/button/Secondary";
+import ButtonTertriary from "./pages/examples/button/Tertriary";
+import ButtonWithIcon from "./pages/examples/button/WithIcon";
+import ButtonWithLoader from "./pages/examples/button/WithLoader";
 
-export { ButtonAllVariants, ButtonDanger };
+export {
+  ButtonAllVariants,
+  ButtonDanger,
+  ButtonDisabled,
+  ButtonPrimary,
+  ButtonSecondary,
+  ButtonTertriary,
+  ButtonWithIcon,
+  ButtonWithLoader,
+};
--- website/components/code/code-previews.tsx
+++ website/components/code/code-previews.tsx
@@ -1,41 +1,20 @@
 /* import dynamic from "next/dynamic"; */
-
-import AllButtonVariants from "../../pages/examples/button/all-variants";
-import ButtonDanger from "../../pages/examples/button/danger";
-import ButtonDisabled from "../../pages/examples/button/disabled";
-import ButtonPrimary from "../../pages/examples/button/primary";
-import ButtonSecondary from "../../pages/examples/button/secondary";
-import ButtontTertiary from "../../pages/examples/button/tertiary";
-import ButtonWithIcon from "../../pages/examples/button/with-icon";
-import ButtonWithLoader from "../../pages/examples/button/with-loader";
 import * as T from "../../component-examples";
+import kebabCase from "lodash.kebabcase";
 
 // TODO: implement such that one can infer the code with dynamic imports
 // TODO: is dynamic imports needed?
-
-const Examples = {
-  "button-all-variants": AllButtonVariants /* dynamic(
-    () => import("../../pages/examples/button/all-variants")
-  ), */,
-  "button-danger": ButtonDanger,
-  "button-disabled": ButtonDisabled,
-  "button-primary": ButtonPrimary,
-  "button-secondary": ButtonSecondary,
-  "button-tertiary": ButtontTertiary,
-  "button-with-icon": ButtonWithIcon,
-  "button-with-loader": ButtonWithLoader,
-};
-
-const E = Object.keys(T).reduce((prev, y) => {
-  return { ...prev, [y]: T[y] };
+const Examples = Object.keys(T).reduce((prev, y) => {
+  return { ...prev, [kebabCase(y)]: T[y] };
 }, {});
 
 export const CodePreviews = (url) => {
-  console.log(E);
+  console.log(Examples);
 
   if (!url || !(url in Examples)) {
     return null;
   }
+
   const Comp = Examples[url];
   return <Comp />;
 };
