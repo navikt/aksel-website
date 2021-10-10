baaeb9192857ee4a963281a4099e05d260a23230
Ken <ken.aleksander@gmail.com>
2021-10-10 20:20:12 +0200

    .

--- website/components/code/code-previews.tsx
+++ website/components/code/code-previews.tsx
@@ -4,13 +4,11 @@ import kebabCase from "lodash.kebabcase";
 
 // TODO: implement such that one can infer the code with dynamic imports
 // TODO: is dynamic imports needed?
-const Examples = Object.keys(T).reduce((prev, y) => {
+export const Examples = Object.keys(T).reduce((prev, y) => {
   return { ...prev, [kebabCase(y)]: T[y] };
 }, {});
 
 export const CodePreviews = (url) => {
-  console.log(Examples);
-
   if (!url || !(url in Examples)) {
     return null;
   }
