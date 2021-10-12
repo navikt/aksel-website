9e212dbce23d9dd1ab3e957597762d8fd140bc0c
Ken <ken.aleksander@gmail.com>
2021-10-12 15:49:46 +0200

    oppdatert sanity struct

--- sanity/schemas/documents/god-praksis/gp-article-page.js
+++ sanity/schemas/documents/god-praksis/gp-article-page.js
@@ -1,6 +1,7 @@
 import { documentMetadata } from "../templates/document-metadata-template";
 import { documentInformation } from "../templates/document-template";
 import { defaultPreview } from "../templates/document-preview-template";
+import { situations } from "./situations";
 
 const prefix = "god-praksis/side/";
 
@@ -12,6 +13,40 @@ export default {
   fields: [
     documentMetadata("article"),
     ...documentInformation(prefix),
+    {
+      title: "Situasjoner og faser",
+      name: "categories",
+      type: "object",
+      fields: [
+        {
+          title: "Situasjon",
+          name: "situation",
+          type: "array",
+          of: [{ type: "string" }],
+          options: {
+            list: [
+              ...situations.map((x) => ({ title: x.name, value: x.name })),
+            ],
+          },
+        },
+        ...situations.map((x) => ({
+          title: `Faser for ${x.name}`,
+          name: `${x.name}_phase`,
+          type: "array",
+          of: [{ type: "string" }],
+          options: {
+            list: [
+              ...x.phases.map((y) => ({
+                title: `Fase ${y.phase}: ${y.name}`,
+                value: y.name,
+              })),
+            ],
+          },
+          hidden: ({ parent }) =>
+            !parent?.situation || parent?.situation.indexOf(x.name) === -1,
+        })),
+      ],
+    },
     {
       name: "body",
       type: "gp_blockContent",
---
+++ sanity/schemas/documents/god-praksis/situations.js
@@ -0,0 +1,51 @@
+export const situations = [
+  {
+    name: "produktutvikling",
+    phases: [
+      { phase: 1, name: "tidlig" },
+      { phase: 2, name: "middels" },
+      { phase: 3, name: "slutt" },
+    ],
+  },
+  {
+    name: "brukerinnsikt",
+    phases: [
+      { phase: 1, name: "veldig tidlig" },
+      { phase: 2, name: "tidlig" },
+      { phase: 3, name: "middels" },
+      { phase: 4, name: "slutt" },
+      { phase: 5, name: "etterlevelse" },
+    ],
+  },
+  {
+    name: "innhold",
+    phases: [
+      { phase: 1, name: "tidlig" },
+      { phase: 2, name: "middels" },
+      { phase: 3, name: "slutt" },
+      { phase: 4, name: "etterlevelse" },
+    ],
+  },
+  {
+    name: "design",
+    phases: [
+      { phase: 1, name: "tidlig" },
+      { phase: 2, name: "før release" },
+      { phase: 3, name: "middels" },
+      { phase: 4, name: "slutt" },
+    ],
+  },
+  {
+    name: "utvikling",
+    phases: [
+      { phase: 1, name: "innsikt" },
+      { phase: 2, name: "tidlig" },
+      { phase: 3, name: "før prosjektstart" },
+      { phase: 4, name: "middels" },
+      { phase: 5, name: "slutt" },
+      { phase: 6, name: "etterlevelse" },
+    ],
+  },
+];
+
+export const phases = {};
