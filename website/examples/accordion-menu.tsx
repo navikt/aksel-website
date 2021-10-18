68b075a58a4e7c937d9b14160349af775bed1d13
Ken <ken.aleksander@gmail.com>
2021-10-18 17:22:34 +0200

    oppdatert eksempel

--- website/examples/accordion-menu.tsx
+++ website/examples/accordion-menu.tsx
@@ -3,21 +3,39 @@ import { AccordionMenu } from "@navikt/ds-react";
 
 export const AccordionMenuExample = () => (
   <>
+    {/*
+
+
+
+
+
+
+
+
+
+
+
+ */}
     <AccordionMenu>
-      <AccordionMenu.Item href="#leo">Leo</AccordionMenu.Item>
-      <AccordionMenu.Collapsable title="Proin">
+      <AccordionMenu.Item href="#leo">Satser</AccordionMenu.Item>
+      <AccordionMenu.Item href="#leo">Regelverk</AccordionMenu.Item>
+      <AccordionMenu.Collapsable title="Medlemskap i folketrygden">
         <AccordionMenu.Item href="#nulla" active>
           Nulla
         </AccordionMenu.Item>
         <AccordionMenu.Item href="#luctus">Luctus</AccordionMenu.Item>
       </AccordionMenu.Collapsable>
-      <AccordionMenu.Collapsable title="Accumsan">
+      <AccordionMenu.Collapsable title="Skjema og søknad">
         <AccordionMenu.Item href="#justo" aria-current="page">
-          Justo
+          Se hva som er endret
         </AccordionMenu.Item>
-        <AccordionMenu.Collapsable title="Proin">
-          <AccordionMenu.Item href="#nulla">Nulla</AccordionMenu.Item>
-          <AccordionMenu.Item href="#luctus">Luctus</AccordionMenu.Item>
+        <AccordionMenu.Collapsable title="Relatert innhold">
+          <AccordionMenu.Item href="#nulla">
+            Saksbehandlingstider
+          </AccordionMenu.Item>
+          <AccordionMenu.Item href="#luctus">
+            Meld fra om endringer
+          </AccordionMenu.Item>
         </AccordionMenu.Collapsable>
       </AccordionMenu.Collapsable>
     </AccordionMenu>
