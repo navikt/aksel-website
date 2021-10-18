b75c6eaea5c796abd22123085e8ee7ec83d2651d
Ken <ken.aleksander@gmail.com>
2021-10-18 14:41:07 +0200

    sletter nå eldre eksempler

--- website/lib/update-code-examples.tsx
+++ website/lib/update-code-examples.tsx
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const updateExamples = () => {
+const updateExamples = async () => {
   const token = process.env.SANITY_WRITE_KEY;
 
   ExampleKeys.forEach((key) => {
@@ -24,20 +24,27 @@ const updateExamples = () => {
   });
 };
 
-updateExamples();
+const deleteRemovedExamples = async () => {
+  const token = process.env.SANITY_WRITE_KEY;
+  const query = `*[_type == "ds_code_example"]`;
+  const docs = await codeExamplesClient(token).fetch(query);
 
-/*
+  docs.forEach((doc) => {
+    if (!ExampleKeys.includes(doc._id.replace("_autogen_example", ""))) {
+      codeExamplesClient(token)
+        .delete(doc._id)
+        .then(() => console.log(`Deleted ${doc._id}`))
+        .catch((e) =>
+          console.error(`Failed deleting ${doc._id}\n ${e.message}`)
+        );
+    }
+  });
+};
 
-/* feedbackClient(token)
-.create({
-  "_createdAt": "2021-10-18T10:34:38Z",
-  "_id": "drafts.6856e03a-2da3-4b80-8b7f-d17566a2209f",
-  "_rev": "iq20f1-ebr-jwq-p2m-js4d57roa",
-  "_type": "ds_code_example",
-  "_updatedAt": "2021-10-18T10:41:38.331Z",
-  "infercode": true,
-  "preview": "https://verktoykasse.sanity.studio/desk/designsystemet;kodeEksempler;6856e03a-2da3-4b80-8b7f-d17566a2209f%2Ctemplate%3Dds_code_example",
-  "title": "Button med error"
-})
+const main = async () => {
+  await deleteRemovedExamples();
 
-*/
+  await updateExamples();
+};
+
+main();
