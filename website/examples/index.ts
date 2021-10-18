f94c81c2b8a014b331ece46e8d73a44cc6849757
Ken <ken.aleksander@gmail.com>
2021-10-18 16:45:34 +0200

    accordion example

--- website/examples/index.ts
+++ website/examples/index.ts
@@ -3,11 +3,13 @@ import kebabCase from "lodash.kebabcase";
 import * as Button from "./button";
 import * as Alert from "./alert";
 import * as AccordionMenu from "./accordion-menu";
+import * as Accordion from "./accordion";
 
 const T = {
   ...Button,
   ...Alert,
   ...AccordionMenu,
+  ...Accordion,
 };
 
 // TODO: implement such that one can infer the code in preview with dynamic imports
