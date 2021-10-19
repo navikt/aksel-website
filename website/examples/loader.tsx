5aa420d9a9179282d955c07a3a7086ecc28d253d
Ken <ken.aleksander@gmail.com>
2021-10-19 20:49:34 +0200

    fikset loader-eksempel

--- website/examples/loader.tsx
+++ website/examples/loader.tsx
@@ -10,14 +10,14 @@ export const LoaderExample = () => (
 LoaderExample.react = `<Loader title="Laster inn ny data..." />`;
 
 export const LoaderAllSizes = () => (
-  <>
+  <div>
     <Loader size="2xlarge" />
     <Loader size="xlarge" />
     <Loader size="large" />
     <Loader size="medium" />
     <Loader size="small" />
     <Loader size="xsmall" />
-  </>
+  </div>
 );
 LoaderAllSizes.react = `<Loader size="2xlarge" />
 <Loader size="xlarge" />
@@ -27,11 +27,11 @@ LoaderAllSizes.react = `<Loader size="2xlarge" />
 <Loader size="xsmall" />`;
 
 export const LoaderAllVariants = () => (
-  <>
+  <div>
     <Loader size="2xlarge" variant="neutral" />
     <Loader size="2xlarge" variant="interaction" />
     <Loader size="2xlarge" variant="inverted" />
-  </>
+  </div>
 );
 LoaderAllVariants.react = `<Loader size="2xlarge" variant="neutral" />
 <Loader size="2xlarge" variant="interaction" />
