21aa35af2290312b7a8138f801308c4a6317275c
Ken <ken.aleksander@gmail.com>
2021-10-21 09:52:21 +0200

    oppdatert script

--- sanity/scripts/renameField.js
+++ sanity/scripts/renameField.js
@@ -1,9 +1,19 @@
 /* eslint-disable no-console */
-import client from "part:@sanity/base/client";
-
-/* Script for å rename data-felt
+const SanityConfig = require("../sanity.json");
+const sanityClient = require("@sanity/client");
+/**  Script for å rename data-felt
  * https://www.sanity.io/schemas/rename-a-field-across-documents-5cd6f5f0
+ * Husk å kjøre backup med sanity dataset export først!
+ * run: sanity exec scripts/renameField.js --with-user-token
  */
+
+const client = sanityClient({
+  projectId: SanityConfig.api.projectId,
+  dataset: SanityConfig.api.dataset,
+  apiVersion: "2020-06-19",
+  useCdn: false,
+});
+
 const fetchDocuments = () =>
   client.fetch(
     `*[_type == 'ds_component_page' && defined(spesific_component)][0...100] {_id, _rev, spesific_component}`
