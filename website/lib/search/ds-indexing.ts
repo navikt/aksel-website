d07b579be479d117d36c078529a66529c6a610aa
Ken <ken.aleksander@gmail.com>
2021-10-25 14:30:49 +0200

    indexing for designsystemet laget

---
+++ website/lib/search/ds-indexing.ts
@@ -0,0 +1,139 @@
+import { feedbackClient } from "..";
+import {
+  DsArticlePage,
+  DsComponentPage,
+  DsTabbedArticlePage,
+} from "../autogen-types";
+import { flattenBlocks } from "sanity-algolia";
+
+const dsIndexingQuery = `*[_type == 'ds_navigation'][0] {
+  headings[]{
+    title,
+    menu[]{
+      ...link->{...},
+    }
+  }
+}`;
+
+const token = process.env.SANITY_WRITE_KEY;
+
+const getDesignsystemRecords = async () => {
+  const sanity = feedbackClient(token);
+  const nav = await sanity.fetch(dsIndexingQuery);
+
+  const records: Record<string, any>[] = [];
+
+  if (!nav.headings) return [];
+
+  nav.headings.forEach((head) => {
+    const category: string = head.title;
+    const menu: (DsComponentPage | DsArticlePage | DsTabbedArticlePage)[] =
+      head.menu;
+
+    menu.forEach((page) => {
+      switch (page._type) {
+        case "ds_article_page":
+          records.push({
+            objectID: `${page._id}`,
+            category,
+            title: page.heading,
+            page: "artikkel",
+            path: page.slug.current,
+            content: `${page.ingress ?? ""} ${flattenBlocks(page.body)}`,
+          });
+          break;
+        case "ds_tabbed_article_page":
+          records.push(...tabbedRecords(page, category));
+          break;
+        case "ds_component_page":
+          records.push(...componentRecords(page, category));
+          break;
+        default:
+          break;
+      }
+    });
+  });
+
+  return records;
+};
+
+export default getDesignsystemRecords;
+
+const tabbedRecords = (page: DsTabbedArticlePage, category: string) => {
+  const records: Record<string, any>[] = [];
+  const title = page.heading;
+  const path = page.slug.current;
+  const id = page._id;
+
+  page.tabs.forEach((tab, x) => {
+    records.push({
+      objectID: `${id}_${tab.title}`,
+      category,
+      title,
+      page: tab.title,
+      path,
+      content:
+        x === 0
+          ? `${page.ingress ?? ""} ${flattenBlocks(tab.body)}`
+          : flattenBlocks(tab.body),
+    });
+  });
+
+  return records;
+};
+
+const componentRecords = (page: DsComponentPage, category: string) => {
+  const records: Record<string, any>[] = [];
+
+  const title = page.heading;
+  const path = page.slug.current;
+  const id = page._id;
+
+  page.usage &&
+    records.push({
+      objectID: `${id}_bruk`,
+      category,
+      title,
+      page: "bruk",
+      path,
+      content: `${page.ingress ?? ""} ${flattenBlocks(page.usage)}`,
+    });
+  page.design &&
+    records.push({
+      objectID: `${id}_design`,
+      category,
+      title,
+      page: "design",
+      path: `${path}/design`,
+      content: flattenBlocks(page.design),
+    });
+  page.development &&
+    records.push({
+      objectID: `${id}_utvikling`,
+      category,
+      title,
+      page: "utvikling",
+      path: `${path}/utvikling`,
+      content: flattenBlocks(page.development),
+    });
+  page.accessibility &&
+    records.push({
+      objectID: `${id}_tilgjengelighet`,
+      category,
+      title,
+      page: "tilgjengelighet",
+      path: `${path}/tilgjengelighet`,
+      content: flattenBlocks(page.accessibility),
+    });
+  !!page.development?.find((x) => x._type === "prop_table") &&
+    records.push({
+      objectID: `${id}_props`,
+      category,
+      title,
+      page: "props",
+      path: `${path}/utvikling`,
+      content: "proptable, api, props, types",
+    });
+
+  return records;
+};
--- website/pages/api/searchHook.ts
+++ website/pages/api/searchHook.ts
@@ -1,11 +1,9 @@
 // pages/api/hook.js
 import { isValidRequest } from "@sanity/webhook";
 import algoliasearch from "algoliasearch";
-import { SanityDocumentStub } from "@sanity/client";
 import type { NextApiRequest, NextApiResponse } from "next";
-import indexer from "sanity-algolia";
-import { feedbackClient } from "../../lib";
 import { isDevelopment } from "../../components";
+import { getDesignsystemRecords } from "../../lib";
 
 const secret = process.env.HOOK_SECRET;
 const token = process.env.SANITY_WRITE_KEY;
@@ -25,119 +23,17 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   }
 
   const algolia = algoliasearch(APP_ID, ALGOLIA_ADMIN);
-
-  const sanity = feedbackClient(token);
-
-  // Configure this to match an existing Algolia index name
   const algoliaIndex = algolia.initIndex("vk_designsystemet");
-
-  const components = await sanity.fetch(componentQuery);
 
   const records: Record<string, any>[] = [];
 
-  components.forEach((comp) => {
-    const title = comp.title;
-    const path = comp.path;
-    const id = comp.objectID;
+  const dsRecords: Record<string, any>[] = await getDesignsystemRecords();
 
-    comp.bruk &&
-      records.push({
-        objectID: `${id}_bruk`,
-        title,
-        page: "bruk",
-        path,
-        content: comp.bruk + comp.ingress,
-      });
-    comp.design &&
-      records.push({
-        objectID: `${id}_design`,
-        title,
-        page: "design",
-        path: `${path}/design`,
-        content: comp.design,
-      });
-    comp.utvikling &&
-      records.push({
-        objectID: `${id}_utvikling`,
-        title,
-        page: "utvikling",
-        path: `${path}/utvikling`,
-        content: comp.utvikling,
-      });
-    comp.tilgjengelighet &&
-      records.push({
-        objectID: `${id}_tilgjengelighet`,
-        title,
-        page: "tilgjengelighet",
-        path: `${path}/tilgjengelighet`,
-        content: comp.tilgjengelighet,
-      });
-    !!comp.proptable?.find((x) => x._type === "prop_table") &&
-      records.push({
-        objectID: `${id}_props`,
-        title,
-        page: "props",
-        path: `${path}/utvikling`,
-        content: "proptable, api, props, types",
-      });
-  });
-  /* console.log(records); */
+  records.push(...dsRecords);
 
   await algoliaIndex.replaceAllObjects(records);
 
   res.status(200).json({ success: true });
-
-  /* const sanityAlgolia = indexer(
-
-    // The second parameter is a function that maps from a fetched Sanity document
-    // to an Algolia Record. Here you can do further mutations to the data before
-    // it is sent to Algolia.
-    (document: SanityDocumentStub) => {
-      switch (document._type) {
-
-        case "ds_component_page":
-          return {
-            ...document,
-            proptable: !!document.proptable?.find(
-              (x) => x._type === "prop_table"
-            ),
-          };
-        default:
-          return document;
-      }
-    },
-
-  ); */
-
-  /* return sanityAlgolia
-    .webhookSync(sanity, req.body)
-    .then(() => res.status(200).send("ok")); */
-
-  /* sanity.fetch(manuaUpdateQuery).then((ids) =>
-    sanityAlgolia
-      .webhookSync(sanity, {
-        ids: { created: ids, updated: ids, deleted: [] },
-      })
-      .then(() => res.status(200).send("ok"))
-  ); */
 }
 
 export default handler;
-
-/* query for manually indexing dataset */
-const manuaUpdateQuery = `*[_type == "ds_component_page" && !(_id in path('drafts.**'))][]._id`;
-
-const componentQuery = `*[_type == "ds_component_page" && !(_id in path('drafts.**'))]{
-  "objectID": _id,
-  "title": heading,
-  ingress,
-  tags,
-  status,
-  "updated": metadata.last_update,
-  "path": slug.current,
-  "bruk": pt::text(usage),
-  "design": pt::text(design),
-  "utvikling": pt::text(development),
-  "tilgjengelighet": pt::text(accessibility),
-  "proptable": development,
-}`;
