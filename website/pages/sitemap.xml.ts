3f87ef0763e807302c28ccc7be387824ee767051
Ken <ken.aleksander@gmail.com>
2021-11-25 21:24:57 +0100

    sitemap generator

---
+++ website/pages/sitemap.xml.ts
@@ -0,0 +1,35 @@
+/* https://github.com/navikt/sosialhjelp-veiviser-ny/blob/291bdd743fc73ecb86f6de701abfa3c0bc64f7f6/pages/sitemap.xml.ts */
+
+import React from "react";
+import { getDsPaths } from "../lib";
+
+const generateSitemap = (pages: string[]): string =>
+  `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${pages
+          .map((slug) => {
+            return `
+                    <url>
+                        <loc>${`https://verktoykassen.vercel.app/${slug}`}</loc>
+                    </url>
+                `;
+          })
+          .join("")}
+    </urlset>
+    `;
+
+class Sitemap extends React.Component {
+  static getInitialProps = async ({ res }) => {
+    // List of posts
+    const pages = await getDsPaths().then((paths) =>
+      paths.map((slugs) => slugs.join("/"))
+    );
+    console.log(pages);
+
+    res.setHeader("Content-Type", "text/xml");
+    res.write(generateSitemap(pages));
+    res.end();
+  };
+}
+
+export default Sitemap;
