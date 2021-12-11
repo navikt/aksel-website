7d89d6b4724a12fff2f09e64c319afa57b49f293
Ken <ken.aleksander@gmail.com>
2021-12-11 22:52:52 +0100

    sjekker om sider hånterer 404 riktig

--- website/cypress/integration/1-page-health/check-health.spec.tsx
+++ website/cypress/integration/1-page-health/check-health.spec.tsx
@@ -4,16 +4,15 @@ import urls from "../../test-urls.json";
 
 describe("Testing health for all pages", () => {
   urls.forEach((url) => {
-    beforeEach(() => {
+    it(`No errorboundary: ${url}`, () => {
       cy.visit(url);
       cy.wait(100);
-    });
-
-    it(`No errorboundary: ${url}`, () => {
       cy.get(".vk-errorboundary").should("not.exist");
     });
 
     it(`Does not return 404: ${url}`, () => {
+      cy.visit(url);
+      cy.wait(100);
       cy.get("#vk-notFoundId").should("not.exist");
     });
   });
