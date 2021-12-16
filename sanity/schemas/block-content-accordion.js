3db6c5bb5f4ffd7bdfb5bd6927d5a4b6dce62502
Ken <ken.aleksander@gmail.com>
2021-12-16 13:15:22 +0100

    la til headings i accordion shcema

--- sanity/schemas/block-content-accordion.js
+++ sanity/schemas/block-content-accordion.js
@@ -1,4 +1,4 @@
-import { block } from "./block-content";
+import { block, TitleRenderer } from "./block-content";
 import { Information } from "@navikt/ds-icons";
 import React from "react";
 
@@ -7,7 +7,26 @@ export default {
   name: "blockContent_accordion",
   type: "array",
   of: [
-    block,
+    {
+      ...block,
+      styles: [
+        {
+          title: "Tittel <h3/>",
+          value: "h3",
+          blockEditor: {
+            render: (props) => TitleRenderer(props, "small", "3"),
+          },
+        },
+        {
+          title: "Tittel <h4/>",
+          value: "heading4",
+          blockEditor: {
+            render: (props) => TitleRenderer(props, "xsmall", "4"),
+          },
+        },
+        ...block.styles,
+      ],
+    },
     {
       type: "tips",
       icon: () => <Information />,
