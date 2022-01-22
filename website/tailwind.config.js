4ede1e67e5ad43741cbb1118646f8f35a53123fa
Ken <ken.aleksander@gmail.com>
2022-01-22 21:21:58 +0100

    fungerende tailwind setup

---
+++ website/tailwind.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
