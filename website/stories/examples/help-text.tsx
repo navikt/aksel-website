9f42bc5e81b4e6565798f563ac2c384dc6f6e4a0
Ken <ken.aleksander@gmail.com>
2022-02-01 10:48:17 +0100

    stories med i tailwind

--- website/stories/examples/help-text.tsx
+++ website/stories/examples/help-text.tsx
@@ -3,7 +3,7 @@ import { HelpText } from "@navikt/ds-react";
 import { ExampleComponent } from "../../lib";
 
 export const HelpTextExample: ExampleComponent = () => (
-  <div className="py-12 px-4">
+  <div className="py-14 pl-6">
     <HelpText title="Hvor kommer dette fra?">
       Informasjonen er hentet fra X sin statistikk fra 2021.
     </HelpText>
