5580085548ffb159283fb67245780eed927dcb64
Ken <ken.aleksander@gmail.com>
2022-02-01 17:51:53 +0100

    oppdatert sanity views

--- sanity/components/user-related-articles.js
+++ sanity/components/user-related-articles.js
@@ -4,7 +4,6 @@ import { getPublishedId } from "part:@sanity/base/util/draft-utils";
 import schema from "part:@sanity/base/schema";
 import React, { useEffect, useCallback, useState } from "react";
 import moment from "moment";
-import { Accordion } from "@navikt/ds-react";
 import { IntentLink } from "part:@sanity/base/router";
 import { Item, List } from "part:@sanity/components/lists/default";
 import SanityPreview from "part:@sanity/base/preview";
@@ -36,16 +35,6 @@ const RelatedArticles = React.forwardRef((props, ref) => {
     getUserDocs();
   }, [getUserDocs]);
 
-  if (userDocuments.length === 0) {
-    return (
-      <Stack space={0} ref={ref} space={[3, 1, 2, 4]}>
-        <Heading as="h2" size={2}>
-          Ingen sider relatert til bruker
-        </Heading>
-      </Stack>
-    );
-  }
-
   const outdated = userDocuments.filter((doc) => {
     if (!doc?.metadata?.last_update) return false;
     const lastUpdate = moment(doc._updatedAt);
@@ -62,92 +51,78 @@ const RelatedArticles = React.forwardRef((props, ref) => {
 
   return (
     <Stack space={0} ref={ref} space={[3, 1, 2, 4]}>
-      <Accordion>
-        {outdated.length > 0 && (
-          <Accordion.Item>
-            <Accordion.Header>Utdaterte sider</Accordion.Header>
-            <Accordion.Content>
-              <List>
-                {outdated.map((doc) => (
-                  <Item key={doc._id}>
-                    <IntentLink
-                      intent="edit"
-                      params={{
-                        type: doc._type,
-                        id: getPublishedId(doc._id),
-                      }}
-                      className={styles.link}
-                    >
-                      <SanityPreview
-                        layout="default"
-                        type={schema.get(doc._type)}
-                        value={doc}
-                        key={doc._id}
-                      />
-                    </IntentLink>
-                  </Item>
-                ))}
-              </List>
-            </Accordion.Content>
-          </Accordion.Item>
-        )}
-
-        {stagnated.length > 0 && (
-          <Accordion.Item>
-            <Accordion.Header>Stagnerte sider sider</Accordion.Header>
-            <Accordion.Content>
-              <List>
-                {stagnated.map((doc) => (
-                  <Item key={doc._id}>
-                    <IntentLink
-                      intent="edit"
-                      params={{
-                        type: doc._type,
-                        id: getPublishedId(doc._id),
-                      }}
-                      className={styles.link}
-                    >
-                      <SanityPreview
-                        layout="default"
-                        type={schema.get(doc._type)}
-                        value={doc}
-                        key={doc._id}
-                      />
-                    </IntentLink>
-                  </Item>
-                ))}
-              </List>
-            </Accordion.Content>
-          </Accordion.Item>
-        )}
-
-        <Accordion.Item>
-          <Accordion.Header>Alle sider</Accordion.Header>
-          <Accordion.Content>
-            <List>
-              {userDocuments.map((doc) => (
-                <Item key={doc._id}>
-                  <IntentLink
-                    intent="edit"
-                    params={{
-                      type: doc._type,
-                      id: getPublishedId(doc._id),
-                    }}
-                    className={styles.link}
-                  >
-                    <SanityPreview
-                      layout="default"
-                      type={schema.get(doc._type)}
-                      value={doc}
-                      key={doc._id}
-                    />
-                  </IntentLink>
-                </Item>
-              ))}
-            </List>
-          </Accordion.Content>
-        </Accordion.Item>
-      </Accordion>
+      <details>
+        <summary>Utdaterte sider: {outdated.length}</summary>
+        <List>
+          {outdated.map((doc) => (
+            <Item key={doc._id}>
+              <IntentLink
+                intent="edit"
+                params={{
+                  type: doc._type,
+                  id: getPublishedId(doc._id),
+                }}
+                className={styles.link}
+              >
+                <SanityPreview
+                  layout="default"
+                  type={schema.get(doc._type)}
+                  value={doc}
+                  key={doc._id}
+                />
+              </IntentLink>
+            </Item>
+          ))}
+        </List>
+      </details>
+      <details>
+        <summary>Stagnerte sider: {stagnated.length}</summary>
+        <List>
+          {stagnated.map((doc) => (
+            <Item key={doc._id}>
+              <IntentLink
+                intent="edit"
+                params={{
+                  type: doc._type,
+                  id: getPublishedId(doc._id),
+                }}
+                className={styles.link}
+              >
+                <SanityPreview
+                  layout="default"
+                  type={schema.get(doc._type)}
+                  value={doc}
+                  key={doc._id}
+                />
+              </IntentLink>
+            </Item>
+          ))}
+        </List>
+      </details>
+      <details>
+        <summary>Alle sider: {userDocuments.length}</summary>
+        <List>
+          {userDocuments.map((doc) => (
+            <Item key={doc._id}>
+              <IntentLink
+                intent="edit"
+                params={{
+                  type: doc._type,
+                  id: getPublishedId(doc._id),
+                }}
+                className={styles.link}
+              >
+                <SanityPreview
+                  layout="default"
+                  type={schema.get(doc._type)}
+                  value={doc}
+                  key={doc._id}
+                />
+              </IntentLink>
+            </Item>
+          ))}
+        </List>
+      </details>
     </Stack>
   );
 });
