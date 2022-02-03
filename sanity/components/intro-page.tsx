08752c6e87d1a6177a3e054782c104ec78b7c871
Ken <ken.aleksander@gmail.com>
2022-02-03 21:32:03 +0100

    bedre intro tekst sanity

--- sanity/components/intro-page.tsx
+++ sanity/components/intro-page.tsx
@@ -1,23 +1,60 @@
-import { BodyLong, Heading } from "@navikt/ds-react";
+import { BodyLong, BodyShort, Heading } from "@navikt/ds-react";
 import { Stack } from "@sanity/ui";
 import { withDocument } from "part:@sanity/form-builder";
-import React, { HTMLAttributes } from "react";
+import React, { HTMLAttributes, useEffect, useState } from "react";
+import userStore from "part:@sanity/base/user";
 
 interface ViewProps extends HTMLAttributes<HTMLDivElement> {}
 
 const IntroPage = React.forwardRef<HTMLDivElement, ViewProps>((props, ref) => {
+  const [user, setUser] = useState<any>({});
+
+  useEffect(() => {
+    const setUserState = async () => {
+      userStore.getCurrentUser().then((user) => setUser(user));
+    };
+    setUserState();
+  }, []);
+
+  console.log(user);
+
   return (
     <Stack ref={ref} space={[3, 1, 2, 4]}>
       <Heading level="2" size="medium" spacing>
-        Velkommen til Aksel CMS
+        {`${
+          user?.name ? `Hei ${user?.name}! ` : ""
+        }Velkommen til Aksel sitt CMS`}
+      </Heading>
+      {user?.roles && (
+        <div>
+          <Heading level="3" size="small" spacing>
+            Du har nå disse rollene:
+          </Heading>
+          {user?.roles.map((x, i) => (
+            <div key={i}>
+              <Heading level="3" size="xsmall" spacing>
+                {x?.title}
+              </Heading>
+              <BodyShort>{x?.description}</BodyShort>
+            </div>
+          ))}
+        </div>
+      )}
+      <div style={{ borderTop: "1px solid #bbb" }} />
+      <Heading level="3" size="small" spacing>
+        Har du ikke de rollene du trenger?
       </Heading>
       <BodyLong>
-        Hvis du logget inn med NAV SSO har du nå bare lese-rettigheter. For å få
-        redigering/publisering-rettigheter må en admin gi deg det.
-      </BodyLong>
-      <BodyLong spacing>
         Hør med Ken Aleksander Johansen på slack, eller bare send en melding til
         oss på #designsystem slack så fikser vi det.
+      </BodyLong>
+      <Heading level="3" size="small">
+        Kom i gang
+      </Heading>
+      <BodyLong>
+        Star med å legge deg selv til som redaktør under "redaktør/bidrag", da
+        er det lettere å holde oversikten over artikler du har vært med å
+        skrive!
       </BodyLong>
       <Heading level="3" size="small">
         Innhold
