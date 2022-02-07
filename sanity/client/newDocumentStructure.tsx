650d516604aa7cc273fb60e04d4c6434554a8438
Ken <ken.aleksander@gmail.com>
2022-02-07 14:39:34 +0100

    bedre config sanity inputTemplates

--- sanity/client/newDocumentStructure.tsx
+++ sanity/client/newDocumentStructure.tsx
@@ -1,3 +1,35 @@
 import S from "@sanity/base/structure-builder";
+const docTypes = [
+  {
+    name: "ds_component_page_template",
+    title: "Komponentside",
+    desc: "Komponentspesifikk artikkel for designsystemet",
+  },
+  {
+    name: "ds_article_page",
+    title: "Artikkel Designsystemet",
+    desc: "Atikkel som legges under designsystemet",
+  },
+  {
+    name: "gp_article_page",
+    title: "Artikkel God-praksis",
+    desc: "Atikkel som legges under god-praksis(wip)",
+  },
+  {
+    name: "ds_changelog",
+    title: "Changelog DS",
+    desc: "Ny changelog for designsystemet",
+  },
+];
 
-export default [];
+export default [
+  ...docTypes.map((doct) =>
+    S.initialValueTemplateItem(doct.name)
+      .id(`doc-${doct.name}`)
+      .title(doct.title)
+      .templateId(doct.name)
+      .description(doct?.desc ?? "")
+  ),
+
+  /* ...S.defaultInitialValueTemplateItems() */
+];
