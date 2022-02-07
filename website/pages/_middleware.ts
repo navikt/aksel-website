b9c57323d6ed8c8df585f72f9daaa6d42217633b
Ken <ken.aleksander@gmail.com>
2022-02-07 18:54:40 +0100

    redirects for designsystem

--- website/pages/_middleware.ts
+++ website/pages/_middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  if (req.url === "http://designsystem.nav.no/") {
+  if (req.url === "https://verktoykasse.dev.nav.no/") {
     return NextResponse.redirect(new URL("/designsystem", req.url));
   }
   return NextResponse.next();
