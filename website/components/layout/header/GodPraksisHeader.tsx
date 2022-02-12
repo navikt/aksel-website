439d6ef2ac6fe3f868bcb5f04320184d75079096
Ken <ken.aleksander@gmail.com>
2022-02-12 16:39:17 +0100

    forenklet gp header

--- website/components/layout/header/GodPraksisHeader.tsx
+++ website/components/layout/header/GodPraksisHeader.tsx
@@ -1,32 +1,11 @@
 import React from "react";
-import { useContext } from "react";
-import { LayoutContext } from "../..";
 import * as S from "./header.styles";
 import PortalNavigation from "./PortalNavigation";
 
-const GodPraksisHeader = (): JSX.Element => {
-  const context = useContext(LayoutContext);
+const GodPraksisHeader = (): JSX.Element => (
+  <S.Header>
+    <PortalNavigation title="God Praksis" />
+  </S.Header>
+);
 
-  const nonMobile = (
-    <>
-      <PortalNavigation title="God Praksis" />
-      {/* <HeaderSearchBar /> */}
-      {/* <S.Link href="#">
-        <Bell
-          focusable={false}
-          aria-label="Notifikasjons ikon"
-          style={{ fontSize: "1.5rem" }}
-        />
-      </S.Link> */}
-    </>
-  );
-
-  const mobile = (
-    <>
-      <PortalNavigation title="God Praksis" />
-    </>
-  );
-
-  return <S.Header>{context.isTablet ? mobile : nonMobile}</S.Header>;
-};
 export default GodPraksisHeader;
