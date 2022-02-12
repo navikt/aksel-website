aff8525d781dc77dab07cd6a839fa6456999b8c0
Ken <ken.aleksander@gmail.com>
2022-02-12 19:56:04 +0100

    portalnav refactor

--- website/components/layout/header/PortalNavigation.tsx
+++ website/components/layout/header/PortalNavigation.tsx
@@ -1,175 +1,15 @@
 import { Back, Expand } from "@navikt/ds-icons";
 import { BodyShort, Label, Popover } from "@navikt/ds-react";
-import { useRouter } from "next/router";
-import React, { useRef, useState } from "react";
-import { useMedia } from "react-use";
-import styled, { css } from "styled-components";
-import { AmplitudeEvents, NavLogoWhite, useAmplitude } from "../..";
+import cl from "classnames";
 import NextLink from "next/link";
-
-export const ScWrapper = styled.div`
-  height: 100%;
-  display: flex;
-  z-index: 1050;
-  margin-right: var(--navds-spacing-8);
-
-  &[data-mobile="true"] {
-    margin-right: 0;
-  }
-`;
-
-const ScPopover = styled(Popover)`
-  border-radius: 4px;
-  border: none;
-  width: 300px;
-  max-width: 100%;
-  z-index: 1100;
-  box-shadow: 0 1px 3px 0 rgba(38, 38, 38, 0.2),
-    0 2px 1px 0 rgba(38, 38, 38, 0.12), 0 1px 1px 0 rgba(38, 38, 38, 0.14);
-
-  :focus {
-    box-shadow: 0 1px 3px 0 rgba(38, 38, 38, 0.2),
-      0 2px 1px 0 rgba(38, 38, 38, 0.12), 0 1px 1px 0 rgba(38, 38, 38, 0.14);
-  }
-
-  > * {
-    border-radius: 4px;
-    background-color: var(--navds-semantic-color-canvas-background-light);
-  }
-
-  > * > li:nth-child(2) {
-    border-top: 1px solid var(--navds-semantic-color-border);
-  }
-
-  ul,
-  li {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-
-  li:first-child > a {
-    border-radius: 4px 4px 0 0;
-  }
-
-  li:last-child > a {
-    border-radius: 0 0 4px 4px;
-  }
-`;
-
-export const ScLinkCss = css`
-  color: var(--navds-semantic-color-text);
-
-  min-height: 5rem;
-  width: 100%;
-  display: flex;
-  text-decoration: none;
-
-  :hover {
-    background-color: var(--navds-semantic-color-canvas-background);
-  }
-
-  :focus {
-    box-shadow: inset 0 0 0 3px var(--navds-semantic-color-focus);
-    z-index: 2;
-    outline: none;
-  }
-`;
-
-export const ScLink = styled.a`
-  ${ScLinkCss}
-  flex-direction: column;
-  padding: 0.75rem 1rem 0.5rem 2rem;
-  align-items: flex-start;
-
-  &[data-active="true"] {
-    border-left: 6px solid
-      var(--navds-semantic-color-canvas-background-inverted);
-    padding-left: calc(2rem - 6px);
-    background-color: var(--navds-semantic-color-canvas-background);
-
-    > :first-child {
-      font-weight: 600;
-    }
-  }
-
-  :focus {
-    box-shadow: inset 0 0 0 3px var(--navds-semantic-color-focus);
-    z-index: 2;
-    outline: none;
-  }
-`;
-
-export const ScIconLink = styled.a`
-  ${ScLinkCss}
-  flex-direction: row;
-  align-items: center;
-  min-height: 3.25rem;
-  gap: 0.5rem;
-  padding: 0.5rem;
-
-  svg {
-    font-size: 1rem;
-  }
-
-  :hover {
-    text-decoration: underline;
-  }
-`;
-
-const ScToggle = styled.button`
-  gap: 1rem;
-
-  @media (max-width: 564px) {
-    min-width: unset;
-  }
-
-  > span {
-    margin-top: 4px;
-    display: flex;
-    gap: 0.5rem;
-    align-items: center;
-
-    svg {
-      font-size: 1rem;
-    }
-  }
-
-  :focus {
-    box-shadow: inset 0 0 0 1px
-        var(--navds-semantic-color-component-background-inverted),
-      inset 0 0 0 3px var(--navds-global-color-blue-200);
-  }
-`;
-
-const ScOverlay = styled.div`
-  width: 100vw;
-  height: 100%;
-  background-color: var(--navds-semantic-color-canvas-background-inverted);
-  opacity: 0;
-  position: fixed;
-  top: var(--header-height);
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  transition: opacity 200ms ease-in-out;
-  visibility: hidden;
-  z-index: 1010;
-
-  &[data-visible="true"] {
-    opacity: 0.5;
-    visibility: visible;
-  }
-`;
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { AmplitudeEvents, NavLogoWhite, useAmplitude } from "../..";
 
 const HeadingDropDown = ({ title }: { title: string }) => {
-  const showLogo = useMedia("(min-width: 563px)", true);
-  const router = useRouter();
-  const [open, setOpen] = useState(false);
-  const buttonRef = useRef(null);
+  const [open, setOpen] = useState(true);
+  const [buttonRef, setButtonRef] = useState(null);
   const { logAmplitudeEvent } = useAmplitude();
-
   const { asPath } = useRouter();
 
   const logNavigation = (e) => {
@@ -182,64 +22,85 @@ const HeadingDropDown = ({ title }: { title: string }) => {
 
   return (
     <>
-      <ScWrapper data-mobile={!showLogo}>
-        <ScToggle
-          ref={buttonRef}
-          className="navdsi-dropdown__toggle navdsi-header__button"
+      <div className="z-[1050] mr-0 flex h-full md:mr-8">
+        <button
+          ref={setButtonRef}
           aria-haspopup="false"
           onClick={() => setOpen(!open)}
           aria-expanded={open}
+          className="navdsi-dropdown__toggle navdsi-header__button gap-4 focus:shadow-[inset_0_0_0_1px_var(--navds-semantic-color-component-background-inverted),inset_0_0_0_3px_var(--navds-global-color-blue-200)]"
         >
-          {showLogo && <NavLogoWhite focusable={false} />}
-          <span>
+          <NavLogoWhite focusable={false} className="hidden md:block" />
+          <span className="mt-1 flex items-center gap-2 text-text-inverted">
             {title}
-            <Expand aria-label="Åpne Portal-navigasjon" />
+            <Expand
+              className="text-medium"
+              aria-label={`${open ? "Lukk" : "Åpne"} portal navigasjon`}
+            />
           </span>
-        </ScToggle>
-
-        <ScPopover
+        </button>
+        <Popover
           onClose={() => setOpen(false)}
-          anchorEl={buttonRef.current}
+          anchorEl={buttonRef}
           open={open}
           arrow={false}
           placement={"bottom-start"}
           offset={8}
-          className="animate-fadeIn"
+          className="z-[1100] w-80 max-w-full animate-fadeIn rounded border-none bg-component-background-light shadow-card"
         >
-          <ul>
+          <ul className="rounded">
             <li>
               <NextLink href="/" passHref>
-                <ScIconLink
+                <a
+                  className="flex items-center gap-2 rounded-t px-2 py-3 hover:bg-gray-50 hover:underline focus:shadow-focus-inset focus:outline-none"
                   onClick={(e) => {
                     setOpen(false);
                     logNavigation(e);
                   }}
                 >
-                  <Back aria-hidden aria-label="Gå til forsiden" />
+                  <Back
+                    className="text-medium"
+                    aria-hidden
+                    aria-label="Gå til forsiden"
+                  />
                   <Label>Tilbake til Aksel</Label>
-                </ScIconLink>
+                </a>
               </NextLink>
             </li>
-            <li>
+            <li className=" border-t border-border">
               <NextLink href="/designsystem" passHref>
-                <ScLink
+                <a
+                  className={cl(
+                    "flex flex-col items-start rounded-b pt-3 pr-4 pb-2 pl-8 hover:bg-gray-100 focus:shadow-focus-inset focus:outline-none",
+                    {
+                      "border-l-[6px] border-canvas-background-inverted bg-canvas-background pl-[26px]":
+                        asPath.startsWith(`/designsystem`),
+                    }
+                  )}
                   onClick={(e) => {
                     setOpen(false);
                     logNavigation(e);
                   }}
-                  data-active={router.asPath.startsWith(`/designsystem`)}
                 >
-                  <BodyShort>Designsystemet</BodyShort>
+                  <BodyShort className="font-semibold">
+                    Designsystemet
+                  </BodyShort>
                   <BodyShort size="small">
                     Informasjon omhandlende designsystemet
                   </BodyShort>
-                </ScLink>
+                </a>
               </NextLink>
             </li>
           </ul>
-        </ScPopover>
-      </ScWrapper>
-      <ScOverlay data-visible={open} />
+        </Popover>
+      </div>
+
+      <div
+        className={cl(
+          "fixed inset-0 z-[1010] h-full w-screen bg-gray-900 transition-opacity",
+          { "visible opacity-50": open, "invisible opacity-0": !open }
+        )}
+      />
     </>
   );
 };
