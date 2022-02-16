f792468fc11ab84b7f6e1d4aef4a4661d6305ccb
Ken <ken.aleksander@gmail.com>
2022-02-16 15:08:56 +0100

    fikset related card issue

--- sanity/schemas/v2/relatert-innhold.tsx
+++ sanity/schemas/v2/relatert-innhold.tsx
@@ -64,7 +64,7 @@ export default {
               name: "tags",
               type: "string",
               initialValue: "default",
-              hidden: ({ parent }) => !parent?.internal,
+              hidden: ({ parent }) => !parent?.intern,
               options: {
                 list: [
                   {
