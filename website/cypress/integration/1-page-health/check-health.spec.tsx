528323cc152aab100307afe0b5b1ca4e652856ae
Ken <ken.aleksander@gmail.com>
2022-02-22 21:31:19 +0100

    faster cypress test

--- website/cypress/integration/1-page-health/check-health.spec.tsx
+++ website/cypress/integration/1-page-health/check-health.spec.tsx
@@ -4,15 +4,10 @@ import urls from "../../test-urls.json";
 
 describe("Testing health for all pages", () => {
   urls.forEach((url) => {
-    it(`No errorboundary: ${url}`, () => {
+    it(`No errorboundary and does not return 404: ${url}`, () => {
       cy.visit(url);
       cy.wait(100);
       cy.get(".vk-errorboundary").should("not.exist");
-    });
-
-    it(`Does not return 404: ${url}`, () => {
-      cy.visit(url);
-      cy.wait(100);
       cy.get("#vk-notFoundId").should("not.exist");
     });
   });
