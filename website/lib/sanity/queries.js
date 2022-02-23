e9442ab607eda99963c3956f35c8e4ed6361fc19
Ken <ken.aleksander@gmail.com>
2022-02-23 16:33:45 +0100

    oppdatert fetching av doks

--- website/lib/sanity/queries.js
+++ website/lib/sanity/queries.js
@@ -303,8 +303,8 @@ export const vkFrontpageQuery = `*[_id == "frontpage_vk_praksis"]
   }
 }`;
 
-export const gpDocuments = `*[_type in ["gp_article_page"]]{ _type, 'slug': slug.current, }`;
-export const akselArtikkelDocuments = `*[_type in ["aksel_artikkel"]]{ _type, 'slug': slug.current, _id }`;
+export const gpDocuments = `*[_type in ["gp_article_page"]]{ _type, 'slug': slug.current, _id }`;
+export const akselArtikkelDocuments = `*[_type in ["aksel_artikkel"]]{ _type, _id, 'slug': slug.current }`;
 
 export const gpDocumentBySlug = `*[slug.current == $slug]
 {
