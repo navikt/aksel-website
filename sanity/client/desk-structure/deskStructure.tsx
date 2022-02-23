5dcf3d6f5815fcbd8676531e9c1ea060ceca1bdf
Ken <ken.aleksander@gmail.com>
2022-02-23 14:19:10 +0100

    sanity preview for gp artikkler

--- sanity/client/desk-structure/deskStructure.tsx
+++ sanity/client/desk-structure/deskStructure.tsx
@@ -4,21 +4,11 @@ import userStore from "part:@sanity/base/user";
 import React from "react";
 import { createSuperPane } from "sanity-super-pane";
 import { ComponentPageWebPreview } from "../../web-previews/ComponentWebPreview";
-import { PageWebPreview } from "../../web-previews/PageWebPreview";
 import { KomponentPreview } from "../../web-previews/KomponentPreview";
+import { PageWebPreview } from "../../web-previews/PageWebPreview";
 import { adminPanel } from "./admin";
 import { dsPanel } from "./ds";
 import { profilePanel } from "./profile";
-
-const sanityClient = require("@sanity/client");
-const SanityConfig = require("../../sanity.json");
-
-const client = sanityClient({
-  projectId: SanityConfig.api.projectId,
-  dataset: SanityConfig.api.dataset,
-  apiVersion: "2020-06-19",
-  useCdn: false,
-});
 
 export const getDefaultDocumentNode = ({ schemaType }) => {
   switch (schemaType) {
@@ -44,6 +34,11 @@ export const getDefaultDocumentNode = ({ schemaType }) => {
         S.view.component(PageWebPreview).title("Preview"),
       ]);
     case "gp_article_page":
+      return S.document().views([
+        S.view.form(),
+        S.view.component(PageWebPreview).title("Preview"),
+      ]);
+    case "aksel_artikkel":
       return S.document().views([
         S.view.form(),
         S.view.component(PageWebPreview).title("Preview"),
--- sanity/web-previews/PageWebPreview.tsx
+++ sanity/web-previews/PageWebPreview.tsx
@@ -8,8 +8,12 @@ export const PageWebPreview = (ctx: any) => {
     return <div>Side må ha en url/slug før den kan forhåndsvises...</div>;
   }
 
+  let previewUrl = `/preview/${slug}`;
+  if (ctx.document.displayed._type === "aksel_artikkel") {
+    previewUrl = `/preview/artikkel/${slug}`;
+  }
+
   const webUrl = "https://design.nav.no";
-  const previewUrl = `/preview/${slug}`;
   const url =
     process.env.NODE_ENV === "production"
       ? webUrl + previewUrl
