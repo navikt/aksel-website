13dcb16d8615713ade719ecc5bec71ee5c61b993
Ken <ken.aleksander@gmail.com>
2022-02-24 14:25:49 +0100

    spacing -> tw

--- website/components/sanity-modules/spacing/index.tsx
+++ website/components/sanity-modules/spacing/index.tsx
@@ -1,17 +1,22 @@
-import styled from "styled-components";
 import { Spacing as SpacingT } from "../../../lib";
-
-const ScSpacing = styled.div<{ $space: string }>`
-  margin-top: ${({ $space }) =>
-    `var(--navds-spacing-${$space.split("-")?.[1]})`};
-`;
+import cl from "classnames";
 
 const Spacing = ({ node }: { node: SpacingT }): JSX.Element => {
   if (!node.space) {
     return null;
   }
 
-  return <ScSpacing aria-hidden="true" $space={node.space} />;
+  return (
+    <div
+      aria-hidden="true"
+      className={cl({
+        "mt-4": node.space === "s-4",
+        "mt-8": node.space === "s-8",
+        "mt-12": node.space === "s-12",
+        "mt-16": node.space === "s-16",
+      })}
+    />
+  );
 };
 
 export default Spacing;
