2b8b89f53da0bf6a05717782af08c9a4c010e716
Ken <ken.aleksander@gmail.com>
2022-02-28 20:11:03 +0100

    satt opp tema-side

--- website/pages/tema/[slug].tsx
+++ website/pages/tema/[slug].tsx
@@ -1,26 +1,37 @@
+import { Heading } from "@navikt/ds-react";
+import Head from "next/head";
 import React from "react";
-import { LayoutPicker, PreviewBanner } from "../../components";
+import { Feedback, PreviewBanner } from "../../components";
 import Footer from "../../components/layout/footer/Footer";
 import AkselHeader from "../../components/layout/header/AkselHeader";
-import {
-  AkselArtikkel,
-  akselDocByTag,
-  akselDocumentBySlug,
-  getAkselArtikler,
-  getAkselTema,
-} from "../../lib";
+import { SanityBlockContent } from "../../components/SanityBlockContent";
+import { akselDocByTag, AkselTema, getAkselTema } from "../../lib";
 import { getClient } from "../../lib/sanity/sanity.server";
 
-const Page = (props: {
-  slug?: string;
-  page: AkselArtikkel;
-  preview: boolean;
-}): JSX.Element => {
-  console.log(props.page);
+const Page = ({ preview, page }: PageProps): JSX.Element => {
+  console.log(page);
   return (
     <>
-      {props.preview && <PreviewBanner />}
-      {/* <LayoutPicker title="Aksel" data={props.page} /> */}
+      {preview && <PreviewBanner />}
+      <Head>
+        <title>{`${page.title} - Aksel`}</title>
+        <meta property="og:title" content={`${page.title} - Aksel`} />
+      </Head>
+      <div className="m-0 w-full max-w-content-w-padding px-4 pb-4 md:mx-auto md:my-0 md:px-6 lg:mx-0 lg:px-12 lg:pb-4">
+        <div className="relative flex flex-col justify-center">
+          <div className="flex max-w-text flex-col items-center pt-8 pb-6">
+            <Heading level="1" size="xlarge" spacing className="index-lvl1">
+              {page.title}
+            </Heading>
+          </div>
+          <SanityBlockContent
+            className="mx-auto mt-12 flex min-h-[500px] w-full flex-col"
+            blocks={page.beskrivelse}
+          />
+
+          <Feedback center docId={page?._id} docType={page?._type} />
+        </div>
+      </div>
     </>
   );
 };
@@ -53,12 +64,14 @@ export const getStaticPaths = async (): Promise<{
   };
 };
 
+interface PageProps {
+  page: AkselTema;
+  slug: string;
+  preview: boolean;
+}
+
 interface StaticProps {
-  props: {
-    page: AkselArtikkel;
-    slug: string;
-    preview: boolean;
-  };
+  props: PageProps;
   notFound: boolean;
   revalidate: number;
 }
