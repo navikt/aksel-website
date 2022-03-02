1cbff1cac5b38c44c039c32fe7e3d26628a408b0
Ken <ken.aleksander@gmail.com>
2022-03-02 13:48:22 +0100

    portal lenker

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,4 +1,4 @@
-import { BodyShort, Heading, Ingress, Link } from "@navikt/ds-react";
+import { BodyShort, Heading, Ingress, Label, Link } from "@navikt/ds-react";
 import Head from "next/head";
 import React, { useEffect } from "react";
 import {
@@ -9,8 +9,12 @@ import {
   useAmplitude,
 } from "../components";
 import Footer from "../components/layout/footer/Footer";
+import AkselHeader from "../components/layout/header/AkselHeader";
 import { VkFrontpage, vkFrontpageQuery } from "../lib";
 import { getClient } from "../lib/sanity/sanity.server";
+import NextLink from "next/link";
+import { Locked, Office1, Star, System, SystemFilled } from "@navikt/ds-icons";
+import cl from "classnames";
 
 const Page = (props: { page: VkFrontpage; preview: boolean }): JSX.Element => {
   const { logAmplitudeEvent } = useAmplitude();
@@ -24,7 +28,7 @@ const Page = (props: { page: VkFrontpage; preview: boolean }): JSX.Element => {
   return (
     <>
       <Head>
-        <title>Aksel</title>
+        <title>Aksel - NAV</title>
         <meta property="og:title" content="Aksel - NAV" />
         <meta
           name="description"
@@ -32,92 +36,56 @@ const Page = (props: { page: VkFrontpage; preview: boolean }): JSX.Element => {
         />
       </Head>
       {props.preview && <PreviewBanner />}
-
-      <Link
-        href="https://old-design-nav.vercel.app/"
-        className="absolute top-4 flex w-[calc(100%_-_2rem)] justify-center text-text md:top-8 md:left-8 md:block md:w-auto"
-      >
-        Gå til gammel dokumentasjon
-      </Link>
-      <div className="mx-auto my-0 flex max-w-screen-lg flex-col items-center justify-center text-center">
-        <div className="sub mb-0 flex items-center md:mb-12">
-          <NAVLogoDark className="justify-self-center text-heading-xlarge" />
-        </div>
-        <Heading spacing level="1" size="xlarge" className="relative w-fit">
-          Aksel
-          <BodyShort className="absolute right-0 top-0 translate-x-full md:-right-2">
-            Beta
-          </BodyShort>
-        </Heading>
-        <Ingress>
-          En samling ressurser fra ulike fagdisipliner som hjelper oss å skape
-          bedre, universelt tilgjengelige og sammenhengende produkter i NAV.
-        </Ingress>
+      <div className="flex h-[700px] w-screen justify-center bg-blue-50">
+        Placeholder
       </div>
-      <nav className="mx-auto mt-16 mb-0" aria-label="Portal navigasjon">
-        <div className="card-grid-flow list-none gap-6 p-0">
-          {props?.page?.cards &&
-            props.page.cards
-              .filter((x) => !x.locked)
-              .map((card) => (
-                <Card
-                  key={card._key}
-                  node={card}
-                  categoryRef={card.category_ref}
-                  href={
-                    card.internal
-                      ? `/${
-                          card.link
-                            ?.split("design.nav.no/")
-                            ?.slice(1)
-                            ?.join("/") ?? card.link
-                        }`
-                      : card.link
-                  }
-                />
-              ))}
-        </div>
-        {props?.page?.cards &&
-          props.page.cards.filter((x) => x.locked).length > 0 && (
-            <>
-              <Heading level="2" size="small" className="mb-8 mt-24">
-                Krever innlogging
-              </Heading>
-              <div className="card-grid-flow list-none gap-6 p-0">
-                {props.page.cards
-                  .filter((x) => x.locked)
-                  .map((card) => (
-                    <Card
-                      key={card._key}
-                      node={card}
-                      categoryRef={card.category_ref}
-                      href={
-                        card.internal
-                          ? `/${
-                              card.link
-                                ?.split("design.nav.no/")
-                                ?.slice(1)
-                                ?.join("/") ?? card.link
-                            }`
-                          : card.link
-                      }
-                    />
-                  ))}
-              </div>
-            </>
-          )}
-      </nav>
+      <div className="flex h-28 w-full items-center justify-center gap-4 bg-gray-200">
+        <NextLink href="/designsystem" passHref>
+          <Label
+            as="a"
+            className="flex items-center gap-3 rounded bg-white px-4 py-2 shadow-card outline-2 hover:underline focus:shadow-focus focus:outline-none"
+          >
+            <SystemFilled className="text-[2rem]" /> Designsystemet
+          </Label>
+        </NextLink>
+        <NextLink href="https://identitet.nav.no" passHref>
+          <Label
+            as="a"
+            className="flex items-center gap-3 rounded bg-white px-4 py-2 shadow-card hover:underline focus:shadow-focus focus:outline-none"
+          >
+            <Office1 className="text-[2rem]" />
+            Identitet
+          </Label>
+        </NextLink>
+
+        <NextLink href="https://sikkerhet.nav.no" passHref>
+          <Label
+            as="a"
+            className={cl(
+              "relative flex items-center gap-3 rounded bg-white px-4 py-2 shadow-card hover:underline focus:shadow-focus focus:outline-none",
+              { "pr-10": true }
+            )}
+          >
+            <Star className="text-[2rem]" />
+            Security playbook
+            <span className="absolute right-0 flex h-full items-center rounded-r bg-gray-500 px-1 text-text-inverted">
+              <Locked />
+            </span>
+          </Label>
+        </NextLink>
+      </div>
     </>
   );
 };
 
 Page.getLayout = (page) => {
   return (
-    <div className="flex w-full flex-col bg-component-background-alternate">
+    <div className="w-full bg-component-background-alternate">
+      <AkselHeader />
       <main
         tabIndex={-1}
         id="hovedinnhold"
-        className="relative flex min-h-screen w-full flex-col px-4 pb-8 pt-12 md:py-12"
+        className="relative flex min-h-screen w-full flex-col pb-8 pt-12 md:py-12"
       >
         {page}
       </main>
