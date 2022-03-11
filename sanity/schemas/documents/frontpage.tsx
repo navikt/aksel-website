c9ebb18b78f59c72ee22eb309a0f14066b19d5b8
Ken <ken.aleksander@gmail.com>
2022-03-11 10:19:40 +0100

    forside schema

--- sanity/schemas/documents/frontpage.tsx
+++ sanity/schemas/documents/frontpage.tsx
@@ -4,61 +4,16 @@ export default {
   type: "document",
   fields: [
     {
-      name: "cards",
-      type: "array",
-      title: "Cards",
-      of: [
-        {
-          title: "Card",
-          name: "card",
-          type: "object",
-          fields: [
-            {
-              title: "Kategori",
-              name: "category_ref",
-              type: "reference",
-              to: [{ type: "main_categories" }],
-              validation: (Rule) =>
-                Rule.required().error("Card må ha en hovedkategori"),
-            },
-            {
-              title: "Lenke url",
-              name: "link",
-              type: "url",
-              validation: (Rule) =>
-                Rule.required().error("Card må ha en lenke url"),
-            },
-            {
-              title: "Side under design.nav.no",
-              name: "internal",
-              type: "boolean",
-              initialValue: true,
-            },
-            {
-              title: "Krever innlogging for tilgang",
-              name: "locked",
-              type: "boolean",
-              initialValue: false,
-            },
-            {
-              title: "Tittel",
-              name: "title",
-              type: "string",
-              validation: (Rule) =>
-                Rule.required().error("Card må ha en Tittel"),
-            },
-            {
-              title: "Innhold",
-              name: "content",
-              type: "string",
-            },
-          ],
-        },
-      ],
+      title: "Tittel",
+      name: "title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: "Beskrivelse",
+      name: "beskrivelse",
+      type: "riktekst",
       validation: (Rule) => Rule.required(),
     },
   ],
-  preview: {
-    prepare: () => ({ title: "Forside Aksel" }),
-  },
 };
