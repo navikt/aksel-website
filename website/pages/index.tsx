852298ce00983c32304979ec1f177aff46b0f942
Ken <ken.aleksander@gmail.com>
2022-03-24 20:28:38 +0100

    oppdatert forside oppsett

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -17,6 +17,23 @@ import {
 } from "../lib";
 import { getClient } from "../lib/sanity/sanity.server";
 
+const RedaksjonsKort = () => (
+  <div className="aksel-layout-x flex w-full justify-center">
+    <div className="flex w-full max-w-2xl flex-col items-center justify-between gap-4 rounded-2xl bg-gray-800 py-6 px-7 text-text-inverted sm:py-12 sm:pl-14 md:flex-row md:gap-0 md:pl-7">
+      <div className="">
+        <Heading size="large" level="2" spacing>
+          Redaksjonen trenger deg!
+        </Heading>
+        <BodyLong>
+          Vi trenger hjelp til å skrive innhold til Aksel. Har du ideer,
+          mulighet til å skrive eller lurer på noe angående innhold? Ta kontakt
+          med oss på Slack.
+        </BodyLong>
+      </div>
+    </div>
+  </div>
+);
+
 const Page = ({ preview, temaer, tekster, bloggs }: PageProps): JSX.Element => {
   const logPortalCard = (e) =>
     logNav(
@@ -24,12 +41,6 @@ const Page = ({ preview, temaer, tekster, bloggs }: PageProps): JSX.Element => {
       window.location.pathname,
       e.currentTarget.getAttribute("href")
     );
-
-  /* const getQuote = () =>
-    quotes.quotes &&
-    quotes.quotes[new Date().getMinutes() % quotes.quotes.length];
-
-  const quote = getQuote(); */
 
   return (
     <>
@@ -42,128 +53,96 @@ const Page = ({ preview, temaer, tekster, bloggs }: PageProps): JSX.Element => {
         />
       </Head>
       {preview && <PreviewBanner />}
-      <div className="aksel-layout-x flex w-full flex-col items-center justify-center pb-24">
-        <Heading
-          level="1"
-          size="xlarge"
-          spacing
-          className="w-full max-w-text sm:text-left"
+      <div className="bg-gray-50">
+        <AkselHeader />
+        <main
+          tabIndex={-1}
+          id="hovedinnhold"
+          className="relative flex min-h-screen-header w-full flex-col items-center py-16 focus:outline-none md:py-24"
         >
-          {tekster.title}
-        </Heading>
-        <SanityBlockContent
-          blocks={tekster.beskrivelse}
-          /* className="text-center" */
-        />
-        {/* {quote && (
-          <>
-            <BodyShort lang="en" className="mb-2 text-center sm:text-left">
-              “{quote.title}”
-            </BodyShort>
-            <BodyShort lang="en" size="small" className="text-text-muted">
-              {quote.kilde}
-            </BodyShort>
-          </>
-        )} */}
-
-        {/* <Search full /> */}
-      </div>
-
-      <div className="flex w-full justify-center bg-gray-200 py-8">
-        <nav
-          aria-label="Portaler til NAV-sider"
-          className="aksel-layout-x mx-auto flex flex-col items-center gap-4 whitespace-nowrap sm:flex-row"
-        >
-          <NextLink href="/designsystem" passHref>
-            <Label
-              as="a"
-              className="flex w-full items-center gap-3 rounded bg-white px-4 py-3 shadow-small outline-2 hover:underline focus:shadow-focus focus:outline-none lg:w-auto"
-              onClick={(e) => logPortalCard(e)}
+          <div className="aksel-layout-x flex w-full flex-col items-center justify-center pb-24">
+            <Heading
+              level="1"
+              size="xlarge"
+              spacing
+              className="w-full max-w-text sm:text-left"
             >
-              <System className="shrink-0 text-[1.5rem]" aria-hidden />
-              Designsystemet
-            </Label>
-          </NextLink>
-          <NextLink href="https://identitet.nav.no" passHref>
-            <Label
-              as="a"
-              className="flex w-full items-center gap-3 rounded bg-white px-4 py-3 shadow-small hover:underline focus:shadow-focus focus:outline-none lg:w-auto"
-              onClick={(e) => logPortalCard(e)}
-            >
-              <Office1 className="shrink-0 text-[1.5rem]" aria-hidden />
-              Identitet
-            </Label>
-          </NextLink>
-
-          <NextLink href="https://sikkerhet.nav.no" passHref>
-            <Label
-              as="a"
-              className="flex w-full items-center gap-3 rounded bg-white px-4 py-3 shadow-small hover:underline focus:shadow-focus focus:outline-none lg:w-auto"
-              onClick={(e) => logPortalCard(e)}
-            >
-              <Star className="shrink-0 text-[1.5rem]" aria-hidden />
-              Security playbook
-            </Label>
-          </NextLink>
-        </nav>
-      </div>
-      {temaer && (
-        <div className="aksel-layout-x flex w-full max-w-7xl flex-col gap-8 py-24">
-          <Heading level="2" size="large">
-            Siste temaer
-          </Heading>
-
-          <nav aria-label="Temasider" className="aksel-card-grid">
-            {temaer.map((tema) => (
-              <TemaCard {...tema} key={tema._id} />
-            ))}
-          </nav>
-        </div>
-      )}
-      {bloggs && bloggs.length > 0 && (
-        <div className="aksel-layout-x flex w-full max-w-7xl flex-col gap-8 pb-24">
-          <Heading level="2" size="large">
-            Siste bloggposts
-          </Heading>
-
-          <nav aria-label="Temasider" className="aksel-card-grid">
-            {bloggs.map((blogg) => (
-              <ArtikkelCard {...blogg} key={blogg._id} />
-            ))}
-          </nav>
-        </div>
-      )}
-      <div className="aksel-layout-x flex w-full justify-center">
-        <div className="flex w-full max-w-2xl flex-col items-center justify-between gap-4 rounded-2xl bg-gray-800 py-6 px-7 text-text-inverted sm:py-12 sm:pl-14 md:flex-row md:gap-0 md:pl-7">
-          <div className="">
-            <Heading size="large" level="2" spacing>
-              Redaksjonen trenger deg!
+              {tekster.title}
             </Heading>
-            <BodyLong>
-              Vi trenger hjelp til å skrive innhold til Aksel. Har du ideer,
-              mulighet til å skrive eller lurer på noe angående innhold? Ta
-              kontakt med oss på Slack.
-            </BodyLong>
+            <SanityBlockContent blocks={tekster.beskrivelse} />
           </div>
-        </div>
+
+          <div className="flex w-full justify-center bg-gray-200 py-8">
+            <nav
+              aria-label="Portaler til NAV-sider"
+              className="aksel-layout-x mx-auto flex flex-col items-center gap-4 whitespace-nowrap sm:flex-row"
+            >
+              <NextLink href="/designsystem" passHref>
+                <Label
+                  as="a"
+                  className="flex w-full items-center gap-3 rounded bg-white px-4 py-3 shadow-small outline-2 hover:underline focus:shadow-focus focus:outline-none lg:w-auto"
+                  onClick={(e) => logPortalCard(e)}
+                >
+                  <System className="shrink-0 text-[1.5rem]" aria-hidden />
+                  Designsystemet
+                </Label>
+              </NextLink>
+              <NextLink href="https://identitet.nav.no" passHref>
+                <Label
+                  as="a"
+                  className="flex w-full items-center gap-3 rounded bg-white px-4 py-3 shadow-small hover:underline focus:shadow-focus focus:outline-none lg:w-auto"
+                  onClick={(e) => logPortalCard(e)}
+                >
+                  <Office1 className="shrink-0 text-[1.5rem]" aria-hidden />
+                  Identitet
+                </Label>
+              </NextLink>
+
+              <NextLink href="https://sikkerhet.nav.no" passHref>
+                <Label
+                  as="a"
+                  className="flex w-full items-center gap-3 rounded bg-white px-4 py-3 shadow-small hover:underline focus:shadow-focus focus:outline-none lg:w-auto"
+                  onClick={(e) => logPortalCard(e)}
+                >
+                  <Star className="shrink-0 text-[1.5rem]" aria-hidden />
+                  Security playbook
+                </Label>
+              </NextLink>
+            </nav>
+          </div>
+
+          {temaer && temaer.length > 0 && (
+            <div className="aksel-layout-x flex w-full max-w-7xl flex-col gap-8 py-24">
+              <Heading level="2" size="large">
+                Siste temaer
+              </Heading>
+
+              <nav aria-label="Temasider" className="aksel-card-grid">
+                {temaer.map((tema) => (
+                  <TemaCard {...tema} key={tema._id} />
+                ))}
+              </nav>
+            </div>
+          )}
+
+          {bloggs && bloggs.length > 0 && (
+            <div className="aksel-layout-x flex w-full max-w-7xl flex-col gap-8 pb-24">
+              <Heading level="2" size="large">
+                Siste bloggposts
+              </Heading>
+
+              <nav aria-label="Temasider" className="aksel-card-grid">
+                {bloggs.map((blogg) => (
+                  <ArtikkelCard {...blogg} key={blogg._id} />
+                ))}
+              </nav>
+            </div>
+          )}
+          <RedaksjonsKort />
+        </main>
+        <Footer />
       </div>
     </>
-  );
-};
-
-Page.getLayout = (page) => {
-  return (
-    <div className="bg-gray-50">
-      <AkselHeader />
-      <main
-        tabIndex={-1}
-        id="hovedinnhold"
-        className="relative flex min-h-screen-header w-full flex-col items-center py-16 focus:outline-none md:py-24"
-      >
-        {page}
-      </main>
-      <Footer />
-    </div>
   );
 };
 
@@ -177,19 +156,16 @@ interface PageProps {
   preview: boolean;
 }
 
-interface StaticProps {
-  props: PageProps;
-  revalidate: number;
-}
-
 export const getStaticProps = async ({
   preview = false,
 }: {
   preview?: boolean;
-}): Promise<StaticProps> => {
-  const temaer = await getClient(preview).fetch(akselTema);
-  const bloggs = await getClient(preview).fetch(akselBloggPosts);
-  const tekster = await getClient(preview).fetch(akselForsideQuery);
+}) => {
+  const client = getClient(preview);
+
+  const temaer = await client.fetch(akselTema);
+  const bloggs = await client.fetch(akselBloggPosts);
+  const tekster = await client.fetch(akselForsideQuery);
 
   return {
     props: {
