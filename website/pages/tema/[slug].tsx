77fe41779868f3374bd7907eb99d019106a5c523
Ken <ken.aleksander@gmail.com>
2022-03-24 20:33:47 +0100

    forenklet tema slug

--- website/pages/tema/[slug].tsx
+++ website/pages/tema/[slug].tsx
@@ -18,71 +18,6 @@ import {
 } from "../../lib";
 import { getClient } from "../../lib/sanity/sanity.server";
 
-const Page = ({ preview, page }: PageProps): JSX.Element => {
-  return (
-    <>
-      {preview && <PreviewBanner />}
-      <Head>
-        <title>{`${page.title} - Aksel`}</title>
-        <meta property="og:title" content={`${page.title} - Aksel`} />
-      </Head>
-      <TemaBreadcrumbs />
-      <Heading level="1" size="xlarge" spacing className="index-lvl1">
-        {page.title}
-      </Heading>
-      <SanityBlockContent blocks={page.beskrivelse} noLastMargin />
-      <Heading level="2" size="large" className="pt-20" spacing>
-        Artikler
-      </Heading>
-      <div className="aksel-card-grid-col-2 ">
-        {page.artikler.map((x) => {
-          const authors = x?.contributors;
-          return (
-            <ArtikkelCard
-              {...x}
-              authors={authors}
-              source={page.title}
-              key={x._id}
-            />
-          );
-        })}
-      </div>
-    </>
-  );
-};
-
-Page.getLayout = (page) => {
-  return (
-    <div className="bg-gray-50">
-      <AkselHeader className="bg-gray-50" />
-      <main
-        tabIndex={-1}
-        id="hovedinnhold"
-        className="aksel-main--start w-full max-w-5xl py-16 md:py-20"
-      >
-        {page}
-      </main>
-      <Footer />
-    </div>
-  );
-};
-
-export const getStaticPaths = async (): Promise<{
-  fallback: string;
-  paths: { params: { slug: string } }[];
-}> => {
-  return {
-    paths: await getAkselTema().then((paths) =>
-      paths.map((slug) => ({
-        params: {
-          slug,
-        },
-      }))
-    ),
-    fallback: "blocking",
-  };
-};
-
 export interface AkselTemaPage extends AkselTema {
   artikler: Partial<
     AkselArtikkel & {
@@ -99,11 +34,61 @@ interface PageProps {
   preview: boolean;
 }
 
-interface StaticProps {
-  props: PageProps;
-  notFound: boolean;
-  revalidate: number;
-}
+const Page = ({ preview, page }: PageProps): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>{`${page.title} - Aksel`}</title>
+        <meta property="og:title" content={`${page.title} - Aksel`} />
+      </Head>
+      {preview && <PreviewBanner />}
+      <div className="bg-gray-50">
+        <AkselHeader className="bg-gray-50" />
+        <main
+          tabIndex={-1}
+          id="hovedinnhold"
+          className="aksel-main--start w-full max-w-5xl py-16 md:py-20"
+        >
+          <TemaBreadcrumbs />
+          <Heading level="1" size="xlarge" spacing className="index-lvl1">
+            {page.title}
+          </Heading>
+          <SanityBlockContent blocks={page.beskrivelse} noLastMargin />
+          <Heading level="2" size="large" className="pt-20" spacing>
+            Artikler
+          </Heading>
+          <div className="aksel-card-grid-col-2 ">
+            {page.artikler.map((x) => {
+              const authors = x?.contributors;
+              return (
+                <ArtikkelCard
+                  {...x}
+                  authors={authors}
+                  source={page.title}
+                  key={x._id}
+                />
+              );
+            })}
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: await getAkselTema().then((paths) =>
+      paths.map((slug) => ({
+        params: {
+          slug,
+        },
+      }))
+    ),
+    fallback: "blocking",
+  };
+};
 
 export const getStaticProps = async ({
   params: { slug },
@@ -111,7 +96,7 @@ export const getStaticProps = async ({
 }: {
   params: { slug: string };
   preview?: boolean;
-}): Promise<StaticProps | { notFound: true }> => {
+}) => {
   const temas = await getClient(true).fetch(akselTemaDocs);
 
   const doc = temas.find((tema) => getTemaSlug(tema?.title) === slug);
