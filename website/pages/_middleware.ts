f0f8322240293e6e238306d11abcfbb345673d05
Ken <ken.aleksander@gmail.com>
2022-03-25 12:51:02 +0100

    middleware test

--- website/pages/_middleware.ts
+++ website/pages/_middleware.ts
@@ -1,15 +1,9 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export async function middleware(req, ev) {
-  /* if (req.pathname === "login") {
-    return NextResponse.next();
+  if (req.nextUrl.hostname === "verktoykasse.dev.nav") {
+    return NextResponse.redirect("https://aksel.nav.no/");
   }
-  const token = req.cookies.token;
 
-  if (!token) {
-    return NextResponse.redirect("/login");
-  } */
-  /* console.log(req.nextUrl); */
   return NextResponse.next();
-  /* return new Response("Hello, world!"); */
 }
