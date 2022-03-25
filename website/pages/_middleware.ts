7626f18834edaf686c1645e0180f22a7c1d0e0da
Ken <ken.aleksander@gmail.com>
2022-03-25 15:12:56 +0100

    oppdatert redirect

--- website/pages/_middleware.ts
+++ website/pages/_middleware.ts
@@ -2,13 +2,8 @@ import { NextResponse, NextRequest } from "next/server";
 
 export async function middleware(req, ev) {
   console.log(req.nextUrl);
-  if (req.nextUrl.href.startsWith("https://verktoykasse.dev.nav.no")) {
-    return NextResponse.redirect(
-      req.nextUrl.href.replace(
-        "https://verktoykasse.dev.nav.no/",
-        "https://aksel.dev.nav.no/"
-      )
-    );
+  if (req.nextUrl.origin.startsWith("https://verktoykasse.dev.nav.no")) {
+    return NextResponse.redirect("https://aksel.dev.nav.no/");
   }
 
   console.log(req.nextUrl.href);
