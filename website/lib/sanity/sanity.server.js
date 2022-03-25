17b4878f5f329d916b6a5c0476af6374a427490f
Ken <ken.aleksander@gmail.com>
2022-03-25 21:44:40 +0100

    fikset workflow for privat dataset

--- website/lib/sanity/sanity.server.js
+++ website/lib/sanity/sanity.server.js
@@ -4,17 +4,20 @@ import { createClient } from "next-sanity";
 const config = {
   projectId: "hnbe3yhs",
   dataset: "production",
-  useCdn: true,
+  useCdn: false,
   apiVersion: "2021-05-31",
 };
 
 // Set up the client for fetching data in the getProps page functions
-export const sanityClient = createClient(config);
+export const sanityClient = createClient({
+  ...config,
+  // eslint-disable-next-line no-undef
+  token: process.env.SANITY_PRIVATE_NO_DRAFTS,
+});
 
 // Set up a preview client with serverless authentication for drafts
 export const previewClient = createClient({
   ...config,
-  useCdn: false,
   // eslint-disable-next-line no-undef
   token: process.env.SANITY_PREVIEW_TOKEN,
 });
@@ -23,7 +26,6 @@ export const previewClient = createClient({
 export const noCdnClient = (token) =>
   createClient({
     ...config,
-    useCdn: false,
     token,
   });
 
