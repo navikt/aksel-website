dff0486b84eb62400b10507ea210c3ed8d4f5623
Ken <ken.aleksander@gmail.com>
2022-03-29 20:33:06 +0200

    justert forside

--- website/pages/designsystem/index.tsx
+++ website/pages/designsystem/index.tsx
@@ -42,39 +42,44 @@ const Page = (props: {
       <main
         tabIndex={-1}
         id="hovedinnhold"
-        className="relative flex min-h-screen-header w-full flex-col bg-component-background-alternate focus:outline-none"
+        className="relative flex justify-center bg-component-background-alternate focus:outline-none"
       >
-        <div className="relative flex w-full bg-canvas-background-light">
-          <div className="flex w-full max-w-[calc(1440px_-_624px)] flex-col items-center py-12 px-4 md:block md:h-[240px] md:py-16 md:px-12">
-            <Heading spacing level="1" size="xlarge">
-              Designsystemet
-            </Heading>
-            <BodyLong>Gjør det enkelt å lage produkter i NAV</BodyLong>
+        <div className="flex min-h-screen-header w-full flex-col items-center">
+          <div className="relative flex w-full justify-center bg-canvas-background-light">
+            <div className="flex w-full max-w-aksel-max-w">
+              <div className="flex w-full max-w-[calc(1440px_-_624px)] flex-col items-center py-12 px-4 md:block md:h-[240px] md:py-16 md:px-12">
+                <Heading spacing level="1" size="xlarge">
+                  Designsystemet
+                </Heading>
+                <BodyLong>Gjør det enkelt å lage produkter i NAV</BodyLong>
+              </div>
+              <DsFrontpageIllustration className="hidden h-[240px] w-[624px] shrink-0 xl:block" />
+            </div>
           </div>
-          <DsFrontpageIllustration className="hidden h-[240px] w-[624px] shrink-0 xl:block" />
+
+          <div className="flex w-full max-w-aksel-max-w flex-col flex-wrap bg-component-background-alternate">
+            <div className="reduced-spacing py-12 px-4 md:px-12 md:py-6">
+              {props?.page?.body && (
+                <SanityBlockContent blocks={props?.page?.body} />
+              )}
+            </div>
+            <div className="flex flex-wrap justify-center gap-6 px-4 pt-0 pb-12 md:justify-start md:px-12 md:pb-16">
+              {props?.page?.cards &&
+                props?.page?.cards.map((card, i) => {
+                  return (
+                    <Card
+                      key={card._key}
+                      node={card as unknown as DsFrontPageCardT}
+                      tag={true}
+                      style={{ animationDelay: `${i * 50}ms` }}
+                      className={`animate-fadeInBottom opacity-0`}
+                    />
+                  );
+                })}
+            </div>
+          </div>
+          <DsFrontpageFooterIllustration className="flex h-full w-full lg:hidden" />
         </div>
-        <div className="flex max-w-screen-2xl flex-col flex-wrap bg-component-background-alternate">
-          <div className="reduced-spacing py-12 px-4 md:px-12 md:py-6">
-            {props?.page?.body && (
-              <SanityBlockContent blocks={props?.page?.body} />
-            )}
-          </div>
-          <div className="flex flex-wrap justify-center gap-6 px-4 pt-0 pb-12 md:justify-start md:px-12 md:pb-16">
-            {props?.page?.cards &&
-              props?.page?.cards.map((card, i) => {
-                return (
-                  <Card
-                    key={card._key}
-                    node={card as unknown as DsFrontPageCardT}
-                    tag={true}
-                    style={{ animationDelay: `${i * 50}ms` }}
-                    className={`animate-fadeInBottom opacity-0`}
-                  />
-                );
-              })}
-          </div>
-        </div>
-        <DsFrontpageFooterIllustration className="flex h-full w-full lg:hidden" />
       </main>
       <Footer />
     </>
