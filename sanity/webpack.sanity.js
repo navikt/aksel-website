8982d9f9dac561c49d1687c6987171eaf1de31a9
Ken <ken.aleksander@gmail.com>
2022-04-01 14:26:17 +0200

    oppdatert sanity

--- sanity/webpack.sanity.js
+++ sanity/webpack.sanity.js
@@ -1,5 +1,7 @@
 module.exports = function (config, options) {
-  config.module.rules[0].exclude =
-    /(node_modules|bower_components)\/(?!@radix-ui)/;
+  config.module.rules[0].exclude = {
+    test: /(node_modules|bower_components)/,
+    not: [/@radix-ui/, /@floating-ui/],
+  };
   return config;
 };
