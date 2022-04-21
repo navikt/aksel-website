1fc4e0fd2a6fd69d56f22b97bd201aabf0968497
Martin Berglund <martin.berglund@nav.no>
2022-04-21 13:09:48 +0200

    Remove linebreaks

--- website/stories/sandbox/accordion.ts
+++ website/stories/sandbox/accordion.ts
@@ -7,10 +7,7 @@ const AccordionSandbox: SandboxComponent = () => {
       Til deg som er mellom 62 og 67 år
     </Accordion.Header>
     <Accordion.Content>
-      Hvis du er mellom 62 og 67 år når du søker, må du
-      som hovedregel ha hatt en pensjonsgivende inntekt
-      som tilsvarer x G, året før du fikk nedsatt
-      arbeidsevnen. NAV kan gjøre unntak.
+      Hvis du er mellom 62 og 67 år når du søker, må du som hovedregel ha hatt en pensjonsgivende inntekt som tilsvarer x G, året før du fikk nedsatt arbeidsevnen. NAV kan gjøre unntak.
     </Accordion.Content>
   </Accordion.Item>
   <Accordion.Item>
@@ -18,10 +15,7 @@ const AccordionSandbox: SandboxComponent = () => {
       Til deg som har yrkesskade eller yrkessykdom
     </Accordion.Header>
     <Accordion.Content>
-      Med yrkesskade mener vi at du har fått en skade som
-      følge av en arbeidsulykke. Vi kan godkjenne en
-      sykdom som yrkessykdom hvis den kommer av skadelig
-      påvirkning fra arbeidsmiljøet.
+      Med yrkesskade mener vi at du har fått en skade som følge av en arbeidsulykke. Vi kan godkjenne en sykdom som yrkessykdom hvis den kommer av skadelig påvirkning fra arbeidsmiljøet.
     </Accordion.Content>
   </Accordion.Item>
 </Accordion>`;
