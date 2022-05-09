6e24b31e0e341fb088c3d0121ae79fe9439c727a
Ken <ken.aleksander@gmail.com>
2022-05-09 19:11:56 +0200

    404 side

--- website/pages/404.tsx
+++ website/pages/404.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
-import Error from "next/error";
 import { AmplitudeEvents, logAmplitudeEvent } from "@/components";
+import AkselLogo from "components/assets/AkselLogo";
+import { BodyLong, Heading, Link } from "@navikt/ds-react";
+import NextLink from "next/link";
 
 function Page() {
   useEffect(() => {
@@ -10,8 +12,22 @@ function Page() {
   }, []);
 
   return (
-    <div id="vk-notFoundId">
-      <Error statusCode={404} title="Fant ikke siden" />
+    <div
+      id="vk-notFoundId"
+      className="flex h-screen w-screen items-center justify-center gap-8"
+    >
+      <AkselLogo width="10rem" />
+      <div>
+        <Heading spacing level="1" size="large">
+          404 - Fant ikke siden
+        </Heading>
+        <BodyLong className="mb-2">
+          Vi fant ikke siden du var ute etter{" "}
+        </BodyLong>
+        <NextLink href="/" passHref>
+          <Link>Tilbake til forsiden</Link>
+        </NextLink>
+      </div>
     </div>
   );
 }
