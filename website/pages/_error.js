2b3ada3f3615d58b4d004adc29b3081c5651b7be
Ken <ken.aleksander@gmail.com>
2022-05-09 19:14:06 +0200

    error page

--- website/pages/_error.js
+++ website/pages/_error.js
@@ -1,8 +1,16 @@
-import NextErrorComponent from "next/error";
+import { Heading } from "@navikt/ds-react";
 
 /* eslint-disable react/prop-types */
 const MyError = ({ statusCode }) => {
-  return <NextErrorComponent statusCode={statusCode} />;
+  return (
+    <div id="vk-notFoundId">
+      <div>
+        <Heading spacing level="1" size="large">
+          {statusCode} - En feil oppstod.
+        </Heading>
+      </div>
+    </div>
+  );
 };
 
 export default MyError;
