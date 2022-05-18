1406459dbc6a76ef454c13ec06fd3040d966faaf
Ken <ken.aleksander@gmail.com>
2022-05-18 14:57:28 +0200

    forside seksjonstyling

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -24,24 +24,7 @@ import NextLink from "next/link";
 import React from "react";
 import { getClient } from "../lib/sanity/sanity.server";
 
-const RedaksjonsKort = () => (
-  <div className="aksel-layout-x flex w-full justify-center">
-    <div className="flex w-full max-w-2xl flex-col items-center justify-between gap-4 rounded-2xl bg-gray-800 py-6 px-7 text-text-inverted xs:flex-row xs:gap-0 xs:pl-7 sm:py-12 sm:pl-14">
-      <div>
-        <Heading size="large" level="2" spacing>
-          Redaksjonen trenger deg!
-        </Heading>
-        <BodyLong>
-          Vi trenger hjelp til å skrive innhold til Aksel. Har du ideer,
-          mulighet til å skrive eller lurer på noe angående innhold? Ta kontakt
-          med oss på Slack.
-        </BodyLong>
-      </div>
-    </div>
-  </div>
-);
-
-const Page = ({ preview, temaer, tekster, bloggs }: PageProps): JSX.Element => {
+const Portaler = () => {
   const logPortalCard = (e) =>
     logNav(
       "portal-kort",
@@ -49,6 +32,137 @@ const Page = ({ preview, temaer, tekster, bloggs }: PageProps): JSX.Element => {
       e.currentTarget.getAttribute("href")
     );
 
+  return (
+    <>
+      <NextLink href="/designsystem">
+        <a
+          onClick={(e) => logPortalCard(e)}
+          className="group flex items-center gap-2 xs:w-auto"
+        >
+          <div className="grid aspect-square w-12 place-items-center rounded-full bg-deepblue-500/80 group-hover:bg-white group-hover:text-deepblue-900">
+            <svg
+              className="h-7 w-7"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+              fill="none"
+              focusable="false"
+              aria-hidden="true"
+            >
+              <path
+                fill="currentColor"
+                d="M6 42v-7.8l9.7-9.7-8.95-9a3.03 3.03 0 0 1-.9-2.1 3.03 3.03 0 0 1 .9-2.1l4.5-4.55a3.03 3.03 0 0 1 2.1-.9 3.07 3.07 0 0 1 2.15.9l9 9 9.85-9.85c.17-.17.33-.28.5-.33.17-.04.35-.07.55-.07.2 0 .38.03.55.08.17.04.33.15.5.32l5.65 5.65c.17.17.28.33.33.5.05.17.07.35.07.55 0 .2-.02.38-.07.55a1.2 1.2 0 0 1-.33.5l-9.85 9.85 9 9a3.07 3.07 0 0 1 .9 2.15 3.03 3.03 0 0 1-.9 2.1l-4.5 4.45a3.03 3.03 0 0 1-2.1.9 3.03 3.03 0 0 1-2.1-.9l-9-8.95L13.8 42H6Zm11.85-19.65 4.5-4.5-3.65-3.65-2.4 2.4-2.1-2.1 2.4-2.4-3.2-3.2-4.5 4.5 8.95 8.95Zm16.7 16.75 4.5-4.5-3.2-3.2-2.4 2.4-2.1-2.1 2.4-2.4-3.65-3.65-4.5 4.5 8.95 8.95ZM9 39h3.5l20.75-20.75-3.5-3.5L9 35.5V39Zm26.35-22.85 3.5-3.5-3.5-3.5-3.5 3.5 3.5 3.5Z"
+              ></path>
+            </svg>
+          </div>
+          <div className="">
+            <Heading level="3" size="xsmall" className="group-hover:underline">
+              Designsystem
+            </Heading>
+            <Detail size="small" className="text-deepblue-100/95">
+              Åpen for alle
+            </Detail>
+          </div>
+        </a>
+      </NextLink>
+      <NextLink href="https://identitet.nav.no/">
+        <a
+          onClick={(e) => logPortalCard(e)}
+          className="group flex items-center gap-2 xs:w-auto"
+        >
+          <div className="grid aspect-square w-12 place-items-center rounded-full bg-deepblue-500/80 group-hover:bg-white group-hover:text-deepblue-900">
+            <svg
+              className="h-7 w-7"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+              fill="none"
+              focusable="false"
+              aria-hidden="true"
+            >
+              <path
+                fill="currentColor"
+                d="M19.25 27.4 21.05 21.65 16.3 17.95H22.1L24 12L25.85 17.95H31.7L26.95 21.65L28.7 27.4L24 23.85ZM12.2 46V30.8Q9.95 28.45 8.975 25.65Q8 22.85 8 20Q8 13.2 12.6 8.6Q17.2 4 24 4Q30.8 4 35.4 8.6Q40 13.2 40 20Q40 22.85 39.025 25.65Q38.05 28.45 35.8 30.8V46L24 42.05ZM24 33Q29.45 33 33.225 29.225Q37 25.45 37 20Q37 14.55 33.225 10.775Q29.45 7 24 7Q18.55 7 14.775 10.775Q11 14.55 11 20Q11 25.45 14.775 29.225Q18.55 33 24 33ZM15.2 41.8 24 39.05 32.8 41.8V33.25Q30.8 34.7 28.5 35.35Q26.2 36 24 36Q21.8 36 19.5 35.35Q17.2 34.7 15.2 33.25ZM24 37.5Q24 37.5 24 37.5Q24 37.5 24 37.5Q24 37.5 24 37.5Q24 37.5 24 37.5Z"
+              ></path>
+            </svg>
+          </div>
+          <div className="">
+            <Heading level="3" size="xsmall" className="group-hover:underline">
+              Identitet
+            </Heading>
+            <Detail size="small" className="text-deepblue-100/95">
+              Åpen for alle
+            </Detail>
+          </div>
+        </a>
+      </NextLink>
+      <NextLink href="https://etterlevelse.intern.nav.no/">
+        <a
+          onClick={(e) => logPortalCard(e)}
+          className="group flex items-center gap-2 xs:w-auto"
+        >
+          <div className="grid aspect-square w-12 place-items-center rounded-full bg-deepblue-500/80 group-hover:bg-white group-hover:text-deepblue-900">
+            <svg
+              className="h-7 w-7"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+              fill="none"
+              focusable="false"
+              aria-hidden="true"
+            >
+              <path
+                fill="currentColor"
+                d="M17.3 45 13.5 38.5 5.95 36.95 6.8 29.6 2 24 6.8 18.45 5.95 11.1 13.5 9.55 17.3 3 24 6.1 30.7 3 34.55 9.55 42.05 11.1 41.2 18.45 46 24 41.2 29.6 42.05 36.95 34.55 38.5 30.7 45 24 41.9ZM24 24ZM21.85 30.65 33.2 19.4 30.95 17.35 21.85 26.35 17.1 21.4 14.8 23.65ZM18.65 41.05 24 38.8 29.5 41.05 32.85 36.05 38.7 34.55 38.1 28.6 42.15 24 38.1 19.3 38.7 13.35 32.85 11.95 29.4 6.95 24 9.2 18.5 6.95 15.15 11.95 9.3 13.35 9.9 19.3 5.85 24 9.9 28.6 9.3 34.65 15.15 36.05Z"
+              ></path>
+            </svg>
+          </div>
+          <div className="">
+            <Heading level="3" size="xsmall" className="group-hover:underline">
+              Etterlevelse
+            </Heading>
+            <Detail size="small" className="text-deepblue-100/95">
+              Kun for ansatte
+            </Detail>
+          </div>
+        </a>
+      </NextLink>
+      <NextLink href="/designsystem">
+        <a
+          onClick={(e) => logPortalCard(e)}
+          className="group flex items-center gap-2 xs:w-auto"
+        >
+          <div className="grid aspect-square w-12 place-items-center rounded-full bg-deepblue-500/80 group-hover:bg-white group-hover:text-deepblue-900">
+            <svg
+              className="h-7 w-7"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+              fill="none"
+              focusable="false"
+              aria-hidden="true"
+            >
+              <path
+                fill="currentColor"
+                d="M24 44Q17 42.25 12.5 35.875Q8 29.5 8 21.9V10L24 4L40 10V21.9Q40 29.5 35.5 35.875Q31 42.25 24 44ZM24 40.9Q29.3 39.15 32.775 34.475Q36.25 29.8 36.85 24H24V7.25L11 12.1V21.9Q11 22.5 11.025 22.925Q11.05 23.35 11.15 24H24Z"
+              ></path>
+            </svg>
+          </div>
+          <div className="">
+            <Heading level="3" size="xsmall" className="group-hover:underline">
+              Security Champions Playbook
+            </Heading>
+            <Detail size="small" className="text-deepblue-100/95">
+              Kun for ansatte
+            </Detail>
+          </div>
+        </a>
+      </NextLink>
+    </>
+  );
+};
+
+const Page = ({ preview, temaer, tekster, bloggs }: PageProps): JSX.Element => {
   const hasPrinsipp1 =
     tekster?.prinsipp_1 &&
     tekster?.prinsipp_1?.hovedside &&
@@ -103,38 +217,7 @@ const Page = ({ preview, temaer, tekster, bloggs }: PageProps): JSX.Element => {
                     Portaler
                   </Heading>
                   <div className="mt-3 grid grid-flow-row justify-items-start gap-4 xs:grid-cols-3 sm:grid-cols-2 md:grid-cols-1">
-                    <NextLink href="/designsystem">
-                      <a className="group flex items-center gap-2 xs:w-auto">
-                        <div className="grid aspect-square w-12 place-items-center rounded-full bg-deepblue-500/80 group-hover:bg-white group-hover:text-deepblue-900">
-                          <svg
-                            className="h-7 w-7"
-                            width="48"
-                            height="48"
-                            viewBox="0 0 48 48"
-                            fill="none"
-                            focusable="false"
-                            aria-hidden="true"
-                          >
-                            <path
-                              fill="currentColor"
-                              d="M6 42v-7.8l9.7-9.7-8.95-9a3.03 3.03 0 0 1-.9-2.1 3.03 3.03 0 0 1 .9-2.1l4.5-4.55a3.03 3.03 0 0 1 2.1-.9 3.07 3.07 0 0 1 2.15.9l9 9 9.85-9.85c.17-.17.33-.28.5-.33.17-.04.35-.07.55-.07.2 0 .38.03.55.08.17.04.33.15.5.32l5.65 5.65c.17.17.28.33.33.5.05.17.07.35.07.55 0 .2-.02.38-.07.55a1.2 1.2 0 0 1-.33.5l-9.85 9.85 9 9a3.07 3.07 0 0 1 .9 2.15 3.03 3.03 0 0 1-.9 2.1l-4.5 4.45a3.03 3.03 0 0 1-2.1.9 3.03 3.03 0 0 1-2.1-.9l-9-8.95L13.8 42H6Zm11.85-19.65 4.5-4.5-3.65-3.65-2.4 2.4-2.1-2.1 2.4-2.4-3.2-3.2-4.5 4.5 8.95 8.95Zm16.7 16.75 4.5-4.5-3.2-3.2-2.4 2.4-2.1-2.1 2.4-2.4-3.65-3.65-4.5 4.5 8.95 8.95ZM9 39h3.5l20.75-20.75-3.5-3.5L9 35.5V39Zm26.35-22.85 3.5-3.5-3.5-3.5-3.5 3.5 3.5 3.5Z"
-                            ></path>
-                          </svg>
-                        </div>
-                        <div className="">
-                          <Heading
-                            level="3"
-                            size="small"
-                            className="group-hover:underline"
-                          >
-                            Designsystem
-                          </Heading>
-                          <Detail className="text-deepblue-100/95">
-                            Åpen for alle
-                          </Detail>
-                        </div>
-                      </a>
-                    </NextLink>
+                    <Portaler />
                   </div>
                 </div>
               </div>
@@ -330,18 +413,20 @@ const Page = ({ preview, temaer, tekster, bloggs }: PageProps): JSX.Element => {
                 {/* Redaksjons-kort */}
                 <div className="grid overflow-hidden rounded-lg shadow xs:sticky xs:top-24">
                   <div className="bg-deepblue-900 px-6 py-6 text-white">
-                    <h2 className="text-2xl font-semibold">
+                    <Heading size="medium" level="2">
                       Aksel trenger deg!
-                    </h2>
-                    <p className="mt-1 max-w-prose">
+                    </Heading>
+                    <BodyLong className="mt-1 max-w-prose">
                       Vi trenger hjelp med å lage innhold til Aksel. Har du
                       ideer, mulighet til å skrive eller lurer på noe om
                       produktutvikling?
-                    </p>
+                    </BodyLong>
                   </div>
                   <a
                     className="group flex items-center justify-between bg-deepblue-200 px-6 py-4 leading-snug transition ease-out hover:bg-deepblue-300 hover:underline"
-                    href="#"
+                    href="https://nav-it.slack.com/archives/C0370ADS0HX"
+                    target="_blank"
+                    rel="noreferrer"
                   >
                     <span>
                       Ta kontakt med{" "}
@@ -351,9 +436,13 @@ const Page = ({ preview, temaer, tekster, bloggs }: PageProps): JSX.Element => {
                 </div>
                 {/* Blogg */}
                 <div className="order-1 mt-16 xs:col-span-2 xs:mt-44">
-                  <h2 className="text-base font-semibold uppercase tracking-widest">
-                    Blogg
-                  </h2>
+                  <Heading
+                    level="2"
+                    size="xsmall"
+                    className="uppercase tracking-widest"
+                  >
+                    Bloggen
+                  </Heading>
                   <div className="mt-1 divide-y divide-gray-200">
                     {/* Blogg-kort */}
                     <div className="grid grid-flow-row-dense grid-cols-[1fr_auto] items-start gap-x-8 py-8">
