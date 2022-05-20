a979523cda032aaf2f715d7989eaf4196193a35b
Ken <ken.aleksander@gmail.com>
2022-05-20 16:13:04 +0200

    elevator js

--- website/components/website-modules/scroll-top/index.tsx
+++ website/components/website-modules/scroll-top/index.tsx
@@ -1,13 +1,54 @@
-import { Up } from "@navikt/ds-icons";
+import { Elevator, Up } from "@navikt/ds-icons";
+import ElevatorJs from "elevator.js";
+import { useEffect, useState } from "react";
 
-const ScrollTop = () => (
-  <button
-    onClick={() => window.scrollTo(0, 0)}
-    className="absolute top-4 right-0 mr-4 mb-2 ml-auto flex min-h-[48px] items-center  gap-2 p-2 text-text-inverted hover:underline focus:shadow-focus-inverted focus:outline-none"
-  >
-    <Up aria-hidden aria-label="Til toppen" />
-    Til toppen
-  </button>
-);
+const ScrollTop = () => {
+  const [isElevator, setIsElevator] = useState(false);
+
+  const handleElevator = (e) => {
+    if ((e.ctrlKey || e.metaKey) && e.shiftKey) {
+      const el = new ElevatorJs({
+        mainAudio: "/sound/elevator.mp3",
+        endAudio: "/sound/ding.mp3",
+      });
+      el?.elevate();
+    } else {
+      window.scrollTo(0, 0);
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      (e.ctrlKey || e.metaKey) && e.shiftKey && setIsElevator(true);
+    };
+
+    const handleKeyUp = (e) => {
+      !((e.ctrlKey || e.metaKey) && e.shiftKey) && setIsElevator(false);
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
+  return (
+    <button
+      onClick={(e) => {
+        handleElevator(e);
+      }}
+      className="absolute top-4 right-0 mr-4 mb-2 ml-auto flex min-h-[48px] items-center  gap-2 p-2 text-text-inverted hover:underline focus:shadow-focus-inverted focus:outline-none"
+    >
+      {isElevator ? (
+        <Elevator aria-label="Til toppen med heismusikk" />
+      ) : (
+        <Up aria-hidden aria-label="Til toppen" />
+      )}
+      Til toppen
+    </button>
+  );
+};
 
 export default ScrollTop;
