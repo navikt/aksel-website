d840eeb85f9014f3a577d003940595ab1c91c3a8
Ken <ken.aleksander@gmail.com>
2022-05-21 15:35:17 +0200

    fjernet gamle ikoner

--- website/components/assets/index.ts
+++ website/components/assets/index.ts
@@ -1,9 +1,5 @@
 export * from "./FigmaIcon";
-export { GithubIcon, GithubIconGrayScale } from "./GithubIcon";
-export { NpmIcon, NpmIconGrayScale } from "./NpmIcon";
 export { default as NavLogoWhite } from "./NavLogoWhite";
 export { default as NAVLogoDark } from "./NavLogoDark";
-export * from "./Canvas";
-export * from "./AlgoliaIcon";
+
 export * from "./DsFrontpageIllustration";
-export * from "./LongArrowRight";
