02b8b1610bdbaf0e5030ce43e29703466c53d942
Ken <ken.aleksander@gmail.com>
2022-06-01 13:02:15 +0200

    oppdatert button sandbox

--- website/stories/sandbox/button.tsx
+++ website/stories/sandbox/button.tsx
@@ -5,15 +5,17 @@ import { SandboxComponentT } from "./types";
 (Button as any).displayName = "Button";
 Star.displayName = "Star";
 const ButtonSandbox: SandboxComponentT = (props: any) => {
+  const newProps = {
+    ...(props?.disabled ? { disabled: true } : {}),
+    ...(props?.loading ? { loading: true } : {}),
+    ...(props?.size === "small" ? { loading: true } : {}),
+  };
   return (
-    <Button
-      variant={props?.variant}
-      size={props?.size}
-      disabled={props?.disabled}
-      loading={props?.loading}
-    >
-      {props?.Komposisjon.includes("Ikon") && <Star />}
+    <Button variant={props?.variant} size={props?.size} {...newProps}>
+      {props?.Komposisjon.startsWith("Ikon") &&
+        !props?.Komposisjon?.endsWith("Ikon") && <Star />}
       {props?.Komposisjon.includes("Tekst") && "Button"}
+      {props?.Komposisjon.endsWith("Ikon") && <Star />}
     </Button>
   );
 };
@@ -24,7 +26,7 @@ ButtonSandbox.args = {
     size: ["medium", "small"],
     disabled: false,
     loading: false,
-    Komposisjon: ["Tekst", "Ikon", "Tekst og Ikon"],
+    Komposisjon: ["Tekst", "Ikon", "Tekst + Ikon", "Ikon + Tekst"],
   },
 };
 
