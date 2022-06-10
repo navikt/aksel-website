28255e1ec8e6dbe53fe0a45743cf253321f4cf77
Ken <ken.aleksander@gmail.com>
2022-06-10 14:48:29 +0200

    textarea stories

--- website/stories/sandbox/textarea.tsx
+++ website/stories/sandbox/textarea.tsx
@@ -9,7 +9,7 @@ const TextareaSandbox: SandboxComponentT = (props: any) => {
 
   const newProps = {
     ...(props?.size ? { size: props?.size } : {}),
-    ...(props?.teller ? { maxLength: 40 } : {}),
+    ...(props?.maxLength ? { maxLength: 40 } : {}),
     ...(props?.error ? { error: "Tilbakemeldingen er for kort." } : {}),
     ...(props?.description
       ? {
@@ -20,7 +20,7 @@ const TextareaSandbox: SandboxComponentT = (props: any) => {
     ...(props?.hideLabel ? { hideLabel: true } : {}),
     ...(props?.disabled ? { disabled: true } : {}),
     ...(props?.maxRows ? { maxRows: 4 } : {}),
-    ...(props?.minRows ? { minRows: 3 } : {}),
+    ...(props?.minRows ? { minRows: 2 } : {}),
   };
 
   return (
@@ -40,10 +40,29 @@ TextareaSandbox.args = {
     hideLabel: false,
     error: false,
     disabled: false,
-    teller: false,
+    maxLength: false,
     minRows: false,
     maxRows: false,
   },
+};
+
+TextareaSandbox.getCode = (props: any) => {
+  const newProps = `${props?.hideLabel ? "\n  hideLabel" : ""}${
+    props?.description
+      ? `\n  description="Vi lagrer bare selve meldingen, ikke hvem som sendte den."`
+      : ""
+  }${props?.error ? `\n  error="Tilbakemeldingen er for kort."` : ""}${
+    props?.disabled ? "\n  disabled" : ""
+  }${props?.maxLength ? "\n  maxLength={40}" : ""}${
+    props?.minRows ? "\n  minRows={4}" : ""
+  }${props?.maxRows ? "\n  maxRows={2}" : ""}`;
+
+  return `<Textarea
+  label="Har du noen tilbakemeldinger?"
+  value={value}
+  onChange={(e) => setValue(e.target.value)}
+  size="${props?.size}"${newProps}
+/>`;
 };
 
 export default TextareaSandbox;
