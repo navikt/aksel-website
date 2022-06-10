3d4d05dcfdd9c43e66c1f2b3b1a6a10437499d01
Ken <ken.aleksander@gmail.com>
2022-06-11 00:19:28 +0200

    tesing og workflow

---
+++ website/jest.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+// jest.config.js
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "node",
+  modulePathIgnorePatterns: ["cypress"],
+  /* setupFiles: ["core-js"], */
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
