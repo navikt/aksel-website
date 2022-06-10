854cebcb3c2d97ee341a2ef1435c10851bc22ee1
Ken <ken.aleksander@gmail.com>
2022-06-10 12:09:11 +0200

    guidepanel story

--- website/stories/sandbox/guidepanel.tsx
+++ website/stories/sandbox/guidepanel.tsx
@@ -19,4 +19,12 @@ GuidePanelSandbox.args = {
   },
 };
 
+GuidePanelSandbox.getCode = (props: any) => {
+  return `<GuidePanel${props?.poster ? " poster" : ""}>
+  Saksbehandlingstiden varierer fra kommune til kommune. Hvis det går mer
+  enn X måneder siden du søkte, skal du få brev om at saksbehandlingstiden
+  er forlenget.
+</GuidePanel>`;
+};
+
 export default GuidePanelSandbox;
