cf18b19922d46416a43b419af7a7f3485dd0a4e2
Ken <ken.aleksander@gmail.com>
2022-06-10 11:41:25 +0200

    alert story

--- website/stories/sandbox/alert.tsx
+++ website/stories/sandbox/alert.tsx
@@ -1,13 +1,13 @@
-import { Alert, Heading } from "@navikt/ds-react";
+import { Alert, BodyLong, Heading } from "@navikt/ds-react";
 import { SandboxComponentT } from "./types";
 
 Alert.displayName = "Alert";
 (Heading as any).displayName = "Heading";
 
-const AlertSandbox: SandboxComponentT = (props: any) => {
+const getText = (variant) => {
   let text = "Alert";
 
-  switch (props?.variant) {
+  switch (variant) {
     case "success":
       text = "Du har sendt søknaden.";
       break;
@@ -25,32 +25,57 @@ const AlertSandbox: SandboxComponentT = (props: any) => {
     default:
       break;
   }
+  return text;
+};
 
+const AlertSandbox: SandboxComponentT = (props: any) => {
   return (
     <Alert
       variant={props?.variant}
       size={props?.size}
       fullWidth={props?.fullWidth}
       inline={props?.inline}
+      className="w-[90%]"
     >
-      {props?.Komposisjon === "Med heading" && (
-        <Heading spacing size="small" level="3">
-          Melding
-        </Heading>
+      {props?.Komposisjon === "Med heading" ? (
+        <>
+          <Heading spacing size="small" level="3">
+            Melding
+          </Heading>
+          <BodyLong>{getText(props?.variant)}</BodyLong>
+        </>
+      ) : (
+        getText(props?.variant)
       )}
-      {text}
     </Alert>
   );
 };
 
 AlertSandbox.args = {
   props: {
-    variant: ["success", "info", "warning", "error"],
+    variant: ["info", "success", "warning", "error"],
     size: ["medium", "small"],
     fullWidth: false,
     inline: false,
     Komposisjon: ["Uten heading", "Med heading"],
   },
+};
+
+AlertSandbox.getCode = (props: any) => {
+  return `<Alert
+  variant="${props?.variant}"
+  size="${props?.size}"${props?.fullWidth ? "\n  fullWidth" : ""}${
+    props?.inline ? "\n  inline" : ""
+  }
+>
+  ${
+    props?.Komposisjon === "Med heading"
+      ? `<Heading spacing size="small" level="3">Melding</Heading>\n  <BodyLong>${getText(
+          props?.variant
+        )}</BodyLong>`
+      : `${getText(props?.variant)}`
+  }
+</Alert>`;
 };
 
 export default AlertSandbox;
