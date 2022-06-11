ca8f81b50d54795a01d6f82cf7e2d37598012e5d
Ken <ken.aleksander@gmail.com>
2022-06-11 20:57:36 +0200

    datestr test

--- website/components/website-modules/utils/__tests__/utils.test.ts
+++ website/components/website-modules/utils/__tests__/utils.test.ts
@@ -1,4 +1,4 @@
-import { isNew } from "../index";
+import { capitalize, dateStr, isNew } from "../index";
 
 describe("Utils test", () => {
   test("isNew", () => {
@@ -7,6 +7,24 @@ describe("Utils test", () => {
 
     expect(isNew(before.toString())).toBeFalsy();
     expect(isNew(after.toString())).toBeTruthy();
+  });
+  test("capitalize", () => {
+    const str1 = "teststr";
+    const str2 = "TESTSTR";
+    const str3 = "tESTSTR";
+    const str4 = "Teststr";
+
+    expect(capitalize(str1)).toEqual("Teststr");
+    expect(capitalize(str2)).toEqual("TESTSTR");
+    expect(capitalize(str3)).toEqual("TESTSTR");
+    expect(capitalize(str4)).toEqual("Teststr");
+  });
+  test("dateStr", () => {
+    const date = "2022-06-09T11:05:48Z";
+    const date2 = "2021-03-02T12:05:48Z";
+
+    expect(dateStr(date)).toEqual("9.juni.2022");
+    expect(dateStr(date2)).toEqual("2.mars.2021");
   });
 });
 
