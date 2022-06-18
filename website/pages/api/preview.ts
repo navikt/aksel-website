d07e5c742df7475d8740994ea33ee9afd4405a5c
Ken <ken.aleksander@gmail.com>
2022-06-18 22:44:20 +0200

    preview lenke funker for forside nå

--- website/pages/api/preview.ts
+++ website/pages/api/preview.ts
@@ -2,11 +2,19 @@ export default function preview(req, res) {
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({});
 
+  console.log(req?.query?.slug);
+
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.writeHead(307, {
-    Location: encodeURI(req?.query?.slug ? `/${req?.query?.slug}` : `/`),
-  });
+  if (req?.query?.slug === ":slug*") {
+    res.writeHead(307, {
+      Location: encodeURI(`/`),
+    });
+  } else {
+    res.writeHead(307, {
+      Location: encodeURI(`/${req?.query?.slug}`),
+    });
+  }
 
   return res.end();
 }
