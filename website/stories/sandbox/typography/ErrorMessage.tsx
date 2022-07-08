a548d973dba3dd38ff105bd62287b53b15fb8901
Ken <ken.aleksander@gmail.com>
2022-07-08 16:43:45 +0200

    fjernet as props

--- website/stories/sandbox/typography/ErrorMessage.tsx
+++ website/stories/sandbox/typography/ErrorMessage.tsx
@@ -2,29 +2,19 @@ import { ErrorMessage } from "@navikt/ds-react";
 import { SandboxComponentT } from "../types";
 
 const ErrorMessageSandbox: SandboxComponentT = (props: any) => {
-  const newProps = {
-    ...(props?.as !== "" ? { as: props?.as } : {}),
-  };
-
-  return (
-    <ErrorMessage {...newProps} size={props?.size}>
-      {props?.tekst}
-    </ErrorMessage>
-  );
+  return <ErrorMessage size={props?.size}>{props?.tekst}</ErrorMessage>;
 };
 
 ErrorMessageSandbox.args = {
   props: {
     size: ["medium", "small"],
     tekst: "Du må fylle ut tekstfeltet før innsending.",
-    as: "",
   },
 };
 
 ErrorMessageSandbox.getCode = (props: any) => {
   return `<ErrorMessage
   ${props?.size !== "medium" ? `size="${props?.size}"` : ""}
-  ${props?.as !== "" ? `as="${props?.as}"` : ""}
 >
   ${props?.tekst}
 </ErrorMessage>`;
