bd0de974c2116a544e8ba5a4d03c36b205d06db8
Ken <ken.aleksander@gmail.com>
2022-07-22 11:25:26 +0200

    copytoclipboard stories

--- website/stories/sandbox/copytoclipboard.tsx
+++ website/stories/sandbox/copytoclipboard.tsx
@@ -1,19 +1,53 @@
 import { Print } from "@navikt/ds-icons";
 import { BodyLong, Link } from "@navikt/ds-react";
+import { CopyToClipboard } from "@navikt/ds-react-internal";
 import { SandboxComponentT } from "./types";
 
 const CopyToClipboardSandbox: SandboxComponentT = (props: any) => {
-  return <div>a</div>;
+  return (
+    <CopyToClipboard
+      popoverText={props?.popoverText}
+      copyText="3.14"
+      iconPosition={props?.iconPosition}
+      size={props?.size}
+      popoverPlacement={props?.popoverPlacement}
+    >
+      {props?.text}
+    </CopyToClipboard>
+  );
 };
 
 CopyToClipboardSandbox.args = {
   props: {
-    ikon: false,
+    size: ["medium", "small"],
+    text: "",
+    popoverText: "Kopierte Pi!",
+    iconPosition: ["left", "right"],
+    popoverPlacement: [
+      "bottom",
+      "top",
+      "right",
+      "left",
+      "top-start",
+      "top-end",
+      "bottom-start",
+      "bottom-end",
+      "right-start",
+      "right-end",
+      "left-start",
+      "left-end",
+    ],
   },
 };
 
 CopyToClipboardSandbox.getCode = (props: any) => {
-  return ``;
+  return `<CopyToClipboard
+  popoverText="${props?.popoverText ? props?.popoverText : "Kopiert!"}"
+  copyText="3.14"
+  iconPosition="${props?.iconPosition}"
+  size="${props?.size}"
+  popoverPlacement="${props?.popoverPlacement}"
+  ${props?.text ? `>\n${props?.text}\n</CopyToClipboard>` : `/>\n`}`;
 };
 
 export default CopyToClipboardSandbox;
