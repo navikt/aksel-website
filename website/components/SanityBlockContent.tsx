d1f2cf749ce697d14b1aac32710679cc8f1e9728
Ken <ken.aleksander@gmail.com>
2022-08-16 13:47:36 +0200

    justert liste-spacing og linjespacing

--- website/components/SanityBlockContent.tsx
+++ website/components/SanityBlockContent.tsx
@@ -152,9 +152,12 @@ const serializers = {
   },
   listItem: (props: any) => {
     return (
-      <li className="ml-5 mb-1 max-w-[calc(theme(spacing.text)_-_1em)]">
+      <BodyLong
+        as="li"
+        className="ml-5 mb-3 max-w-[calc(theme(spacing.text)_-_1em)]"
+      >
         {props.children}
-      </li>
+      </BodyLong>
     );
   },
   marks: {
