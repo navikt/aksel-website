6df33d80a281350a91dfdbdd143e49aade10513c
Ken <ken.aleksander@gmail.com>
2022-09-12 15:53:53 +0200

    oppdatert stepper-story

--- website/stories/sandbox/stepper.tsx
+++ website/stories/sandbox/stepper.tsx
@@ -10,6 +10,9 @@ const StepperSandbox: SandboxComponentT = (props: any) => {
     ...(props?.as === "button" ? { as: "button" } : { href: "#" }),
   };
 
+  const single = props?.["interactive (false)"] === "enkel steg";
+  const all = props?.["interactive (false)"] === "alle steg";
+
   return (
     <>
       <h2 id="stepper-heading" hidden>
@@ -21,11 +24,33 @@ const StepperSandbox: SandboxComponentT = (props: any) => {
         onStepChange={setActiveStep}
         orientation={props?.orientation}
       >
-        <Stepper.Step {...newProps}>Start søknad</Stepper.Step>
-        <Stepper.Step {...newProps}>Saksopplysninger</Stepper.Step>
-        <Stepper.Step {...newProps}>Vedlegg</Stepper.Step>
-        <Stepper.Step {...newProps}>Oppsummering</Stepper.Step>
-        <Stepper.Step {...newProps}>Innsending</Stepper.Step>
+        <Stepper.Step
+          {...newProps}
+          completed={props?.completed}
+          interactive={!all}
+        >
+          Start søknad
+        </Stepper.Step>
+        <Stepper.Step
+          {...newProps}
+          completed={props?.completed}
+          interactive={!all}
+        >
+          Saksopplysninger
+        </Stepper.Step>
+        <Stepper.Step
+          {...newProps}
+          interactive={!single && !all}
+          completed={props?.completed}
+        >
+          Vedlegg
+        </Stepper.Step>
+        <Stepper.Step {...newProps} interactive={!single && !all}>
+          Oppsummering
+        </Stepper.Step>
+        <Stepper.Step {...newProps} interactive={!single && !all}>
+          Innsending
+        </Stepper.Step>
       </Stepper>
     </>
   );
@@ -33,13 +58,21 @@ const StepperSandbox: SandboxComponentT = (props: any) => {
 
 StepperSandbox.args = {
   props: {
-    orientation: ["horizontal", "vertical"],
+    orientation: ["vertical", "horizontal"],
     as: ["link", "button"],
+    "interactive (false)": ["", "enkel steg", "alle steg"],
+    completed: false,
   },
 };
 
 StepperSandbox.getCode = (props: any) => {
   const as = props?.as === "button" ? `as="button"` : `href="#"`;
+  const single = props?.["interactive (false)"] === "enkel steg";
+  const all = props?.["interactive (false)"] === "alle steg";
+
+  const interactiveSingle = single ? `interactive={false}\n` : "";
+  const interactiveAll = all ? `interactive={false}\n` : "";
+  const completed = props?.completed ? `completed\n` : "";
 
   return `
   const [activeStep, setActiveStep] = useState(1);
@@ -52,12 +85,13 @@ StepperSandbox.getCode = (props: any) => {
         activeStep={activeStep}
         onStepChange={(x) => setActiveStep(x)}
         orientation="${props?.orientation}"
+        ${interactiveAll}
       >
-        <Stepper.Step ${as}>Start søknad</Stepper.Step>
-        <Stepper.Step ${as}>Saksopplysninger</Stepper.Step>
-        <Stepper.Step ${as}>Vedlegg</Stepper.Step>
-        <Stepper.Step ${as}>Oppsummering</Stepper.Step>
-        <Stepper.Step ${as}>Innsending</Stepper.Step>
+        <Stepper.Step ${as}${completed}>Start søknad</Stepper.Step>
+        <Stepper.Step ${as}${completed}>Saksopplysninger</Stepper.Step>
+        <Stepper.Step ${as}${interactiveSingle}${completed}>Vedlegg</Stepper.Step>
+        <Stepper.Step ${as}${interactiveSingle}>Oppsummering</Stepper.Step>
+        <Stepper.Step ${as}${interactiveSingle}>Innsending</Stepper.Step>
       </Stepper>
     </>
   );`;
