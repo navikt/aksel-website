6e0924fe3902611b5d4c2102bdc2be9298243773
Ken <ken.aleksander@gmail.com>
2022-09-15 15:39:51 +0200

    finere breadcrumbs

--- website/components/website-modules/breadcrumbs/index.tsx
+++ website/components/website-modules/breadcrumbs/index.tsx
@@ -3,55 +3,25 @@ import NextLink from "next/link";
 const BreadCrumbs = ({ href, text }: { href: string; text: string }) => {
   return (
     <NextLink href={href} passHref>
-      <a className="group mb-1 flex w-fit items-center justify-start gap-3 text-deepblue-500 transition-transform hover:text-deepblue-800 focus:underline focus:outline-none">
+      <a className="group mb-1 flex w-fit items-center justify-start gap-2 text-deepblue-500 transition-transform hover:text-deepblue-800 focus:underline focus:outline-none">
         <svg
-          width="18"
-          height="15"
-          viewBox="0 0 18 15"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
+          className="h-[19px] w-[10px] group-hover:w-[16px]"
         >
           <path
-            d="M5.8 13.5L1.58234 9C0.805887 8.17157 0.805888 6.82843 1.58234 6L5.8 1.5"
-            stroke="currentColor"
+            d="M8 17.5L2.12132 11.6213C0.949744 10.4497 0.949746 8.55025 2.12132 7.37868L8 1.5"
+            stroke="#004367"
             strokeWidth="1.5"
-            strokeLinecap="round"
           />
           <path
-            d="M1.80078 7.5H10.8008"
-            stroke="currentColor"
+            d="M16 9.72266L8 9.72266"
+            stroke="#004367"
             strokeWidth="1.5"
-            strokeLinecap="round"
-            className="transition-transform group-hover:[transform:_rotateY(60deg)]"
-          />
-          <path
-            d="M14.8008 7.5H16.8008"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-          />
-          <path
-            d="M14.8008 7.5H16.8008"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            className="transition-transform group-hover:[transform:_translateX(-2px)]"
-          />
-          <path
-            d="M14.8008 7.5H16.8008"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            className="transition-transform group-hover:[transform:_translateX(-4px)]"
-          />
-          <path
-            d="M14.8008 7.5H16.8008"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            className="transition-transform group-hover:[transform:_translateX(-6px)]"
+            className="hidden group-hover:block"
           />
         </svg>
+
         {text}
       </a>
     </NextLink>
