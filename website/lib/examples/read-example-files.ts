617a1f6dace30d03b26553de8875cf8c47247ac0
Ken <ken.aleksander@gmail.com>
2022-09-19 21:29:21 +0200

    examples fileread

---
+++ website/lib/examples/read-example-files.ts
@@ -0,0 +1,57 @@
+import path from "path";
+import fs from "fs";
+
+/**
+ *
+ * @param dirName Directory name
+ * @returns File-content for files in dir + filename
+ */
+export const ReadExampleFiles = (
+  dirName: string
+): { code: string; name: string; dir: string }[] => {
+  const examplePath = path.resolve(process.cwd(), `pages/examples/${dirName}`);
+  if (fs.existsSync(examplePath)) {
+    const files = fs.readdirSync(examplePath);
+
+    const res = files.map((file) => {
+      let code = "";
+      const filepath = path.resolve(
+        process.cwd(),
+        `pages/examples/${dirName}/${file}`
+      );
+      code = fs.readFileSync(filepath, "utf-8");
+      return {
+        code: code
+          .split("\n")
+          .filter((line) => !line.includes("withDsExample"))
+          .join("\n"),
+        name: file.replace(".tsx", ""),
+        dir: dirName,
+      };
+    });
+    return res;
+  }
+};
+
+/**
+ *
+ * @param fileName example filename including dir: button/primary.tsx
+ * @returns File-content for file + filename
+ */
+export const ReadExampleFile = (
+  fileName: string
+): { code: string; name: string; dir: string } => {
+  const examplePath = path.resolve(process.cwd(), `pages/examples/${fileName}`);
+  if (fs.existsSync(examplePath)) {
+    let code = "";
+    code = fs.readFileSync(examplePath, "utf-8");
+    return {
+      code: code
+        .split("\n")
+        .filter((line) => !line.includes("withDsExample"))
+        .join("\n"),
+      name: fileName.replace(".tsx", ""),
+      dir: fileName.split("/")[0],
+    };
+  }
+};
