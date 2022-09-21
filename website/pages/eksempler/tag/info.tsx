03b322ecb10f7cee9a6c1d69bd7b6d2b1cfbdb3e
Ken <ken.aleksander@gmail.com>
2022-09-21 21:10:12 +0200

    index i eksempler

--- website/pages/eksempler/tag/info.tsx
+++ website/pages/eksempler/tag/info.tsx
@@ -2,7 +2,11 @@ import { Tag } from "@navikt/ds-react";
 import { withDsExample } from "components/website-modules/examples/withDsExample";
 
 const Example = () => {
-  return <Tag variant="success">Success</Tag>;
+  return <Tag variant="info">Info</Tag>;
 };
 
 export default withDsExample(Example);
+
+export const args = {
+  index: 0,
+};
