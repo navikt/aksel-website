ff86e8150a467a62f489995ac4058976ba722790
Ken <ken.aleksander@gmail.com>
2022-09-21 17:24:47 +0200

    rename codeexamples folder

--- website/components/index.ts
+++ website/components/index.ts
@@ -6,7 +6,7 @@ export { default as Anatomi } from "./sanity-modules/AnatomiSeksjon";
 export { default as Bilde } from "./sanity-modules/bilde/Bilde";
 export * from "./sanity-modules/cards";
 export * from "./sanity-modules/code";
-export * from "./sanity-modules/CodeExamples";
+export * from "./sanity-modules/code-examples";
 export { default as DoDont } from "./sanity-modules/DoDont";
 export { default as IntroKomponent } from "./sanity-modules/IntroSeksjon";
 export { default as Kode } from "./sanity-modules/Kode";
