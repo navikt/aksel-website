167a7dac02c67d5918c94044507e5a41ea028def
Ken <ken.aleksander@gmail.com>
2022-09-24 00:08:23 +0200

    :art: Status-tags

--- website/components/sanity-modules/component-overview/index.tsx
+++ website/components/sanity-modules/component-overview/index.tsx
@@ -3,6 +3,36 @@ import { SanityBlockContent } from "@/sanity-block";
 import { Heading } from "@navikt/ds-react";
 import Nextlink from "next/link";
 
+const getStatus = (status?: SanityT.Schema.komponent_artikkel["status"]) => {
+  switch (status?.tag) {
+    case "beta":
+      return (
+        <span className="ml-2 rounded bg-purple-400 px-1 text-sm text-white">
+          Ready
+        </span>
+      );
+    case "new":
+      return (
+        <span className="ml-2 rounded bg-green-300 px-1 text-sm text-gray-900">
+          Ready
+        </span>
+      );
+    case "ready":
+      return (
+        <span className="ml-2 rounded bg-deepblue-400 px-1 text-sm text-white">
+          Ready
+        </span>
+      );
+
+    default:
+      return (
+        <span className="ml-2 rounded bg-deepblue-400 px-1 text-sm text-white">
+          Ready
+        </span>
+      );
+  }
+};
+
 const ComponentOverview = ({
   node,
 }: {
@@ -30,15 +60,18 @@ const ComponentOverview = ({
             <div className="group relative rounded border border-gray-300/60 shadow-small focus-within:shadow-focus hover:shadow-medium">
               <div></div>
               <div className="grid p-3">
-                <Nextlink href={`/${x?.slug}`} passHref>
-                  <Heading
-                    as="a"
-                    size="small"
-                    className="z-10 before:absolute before:inset-0 focus:outline-none group-hover:underline"
-                  >
-                    {x.heading}
-                  </Heading>
-                </Nextlink>
+                <span>
+                  <Nextlink href={`/${x?.slug}`} passHref>
+                    <Heading
+                      as="a"
+                      size="small"
+                      className="z-10 before:absolute before:inset-0 focus:outline-none group-hover:underline"
+                    >
+                      {x.heading}
+                    </Heading>
+                  </Nextlink>
+                  {getStatus(x.status)}
+                </span>
                 <span className="clamp-3 mt-1 overflow-hidden">
                   {<SanityBlockContent noLastMargin blocks={x?.ingress} />}
                 </span>
