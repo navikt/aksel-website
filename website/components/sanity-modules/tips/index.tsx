dd5b1fe5b94d1fe987c7531660c8d9f7598b6948
Ken <ken.aleksander@gmail.com>
2022-09-24 23:22:15 +0200

    :lipstick: Forbedret tips

--- website/components/sanity-modules/tips/index.tsx
+++ website/components/sanity-modules/tips/index.tsx
@@ -36,12 +36,12 @@ const Tips = ({ node }: { node: SanityT.Schema.tips }): JSX.Element => {
             <path
               d="M13 6h-2v5.33L6 18h12l-5-6.67z"
               className="fill-green-200/30"
-            ></path>
+            />
             <path
               className="fill-green-300"
               d="M20.8 18.4 15 10.67V6.5l1.35-1.69c.26-.33.03-.81-.39-.81H8.04c-.42 0-.65.48-.39.81L9 6.5v4.17L3.2 18.4c-.49.66-.02 1.6.8 1.6h16c.82 0 1.29-.94.8-1.6zM6 18l5-6.67V6h2v5.33L18 18H6z"
-            ></path>
-          </svg>{" "}
+            />
+          </svg>
           Hjelp ønskes
         </Detail>
         <SanityBlockContent blocks={node.body} noLastMargin />
@@ -71,16 +71,17 @@ const Tips = ({ node }: { node: SanityT.Schema.tips }): JSX.Element => {
     <div
       className={cl(
         style.tips,
-        "linear my-8 max-w-2xl rounded-r border-l-[6px] border-l-gray-500 px-4 py-4 xs:px-8"
+        "my-7 max-w-2xl px-4 py-3 shadow-[inset_5px_0_0_0_var(--navds-global-color-gray-500),inset_0_0_0_1px_var(--navds-global-color-gray-200)] xs:px-6"
       )}
     >
       <Detail
-        className="flex items-center gap-1 font-semibold text-text-muted"
+        className="override-text-no-max -ml-[2px] flex items-center gap-1 font-semibold text-text-muted"
         spacing
       >
-        <LightBulb className="text-large" aria-label="tips" aria-hidden /> TIPS
+        <LightBulb className="text-large" title="tips" aria-hidden />
+        Tips
       </Detail>
-      <SanityBlockContent blocks={node.body} noLastMargin />
+      <SanityBlockContent blocks={node.body} noLastMargin size="small" />
     </div>
   );
 };
