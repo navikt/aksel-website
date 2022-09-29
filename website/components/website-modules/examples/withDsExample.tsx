bb05f1a9a2074905242976244bb90f5f01c7a901
Ken <ken.aleksander@gmail.com>
2022-09-29 13:39:31 +0200

    :art: loader

--- website/components/website-modules/examples/withDsExample.tsx
+++ website/components/website-modules/examples/withDsExample.tsx
@@ -1,9 +1,10 @@
 import { ComponentType } from "react";
 import styles from "./examples.module.css";
+import cl from "classnames";
 
-export const withDsExample = (Component: ComponentType) => {
+export const withDsExample = (Component: ComponentType, inverted?: boolean) => {
   const DsHOC = (props: any) => (
-    <div className={styles.container}>
+    <div className={cl(styles.container, { "bg-gray-900": inverted })}>
       <div id="ds-example">
         <Component {...props} />
       </div>
---
+++ website/pages/eksempler/loader/inverted.tsx
@@ -0,0 +1,12 @@
+import { Loader } from "@navikt/ds-react";
+import { withDsExample } from "components/website-modules/examples/withDsExample";
+
+const Example = () => {
+  return <Loader size="3xlarge" title="venter..." variant="inverted" />;
+};
+
+export default withDsExample(Example, true);
+
+export const args = {
+  index: 2,
+};
