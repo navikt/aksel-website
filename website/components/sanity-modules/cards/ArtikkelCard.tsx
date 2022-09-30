eba4baa1486d60914e21e16a4c36d62059260cc8
Ken <ken.aleksander@gmail.com>
2022-09-30 13:13:00 +0200

    :lipstick: ring-1 artikkelkort

--- website/components/sanity-modules/cards/ArtikkelCard.tsx
+++ website/components/sanity-modules/cards/ArtikkelCard.tsx
@@ -36,7 +36,7 @@ export const ArtikkelCard = ({
             e.currentTarget.getAttribute("href")
           )
         }
-        className="group relative rounded bg-white p-5 pb-16 shadow-small hover:shadow-medium focus:shadow-focus focus:outline-none "
+        className="group relative rounded bg-white p-5 pb-16 shadow-small ring-1 ring-gray-900/10 hover:shadow-medium focus:shadow-focus focus:outline-none "
       >
         <Heading
           level="2"
