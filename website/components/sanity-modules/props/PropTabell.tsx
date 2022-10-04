ee8dff2cdba344193c03e40ddf08b15bcd49f947
Lars Hansen <lars.eirik.korsgaard.hansen@nav.no>
2022-10-04 10:28:09 +0200

    wip: legge til defaultvalues i propstabell

--- website/components/sanity-modules/props/PropTabell.tsx
+++ website/components/sanity-modules/props/PropTabell.tsx
@@ -27,6 +27,7 @@ const List = ({ prop, parent }: { prop: any; parent: string }) => {
         } `}</span>
         <span>{prop.type ? <>{Highlighter({ type: prop.type })}</> : ""}</span>
       </dt>
+      {prop.defaultValue && <span>default: {prop.defaultValue}</span>}
       {prop.description && (
         <dl className="font-sans text-base">{prop.description}</dl>
       )}
