2fc52bb091dfe6b00f263fcc1117c80fa820155a
Ken <ken.aleksander@gmail.com>
2022-10-11 16:24:13 +0200

    :pencil2: beta-tekst

--- website/components/layout/page-templates/KomponentArtikkel.tsx
+++ website/components/layout/page-templates/KomponentArtikkel.tsx
@@ -131,10 +131,15 @@ const KomponentArtikkelTemplate = ({
                 <Heading level="2" size="small" spacing>
                   Beta
                 </Heading>
-                <BodyLong className="override-text-no-max">
+                <BodyLong className="override-text-no-max mb-2">
                   Komponenten er under utvikling. Dette kan medføre
                   breaking-changes i patch/minor versjon av kodepakker. Vær
                   forsiktig ved bruk og unngå production om mulig.
+                </BodyLong>
+                <BodyLong className="override-text-no-max">
+                  Har du innspill eller funnet en bug? Send oss en melding her
+                  eller på slack! Beta-versjon er ment for rask iterering, så
+                  alle innspill hjelper.
                 </BodyLong>
               </div>
             )}
